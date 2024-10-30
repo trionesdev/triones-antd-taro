4b3be37820f542f18873c3b733d71c926974d913
bane.shi <fengxiaotx@163.com>
2024-10-31 00:04:00 +0800

    feat: init

--- packages/antd-taro-react/src/TabBar/tab-bar.tsx
+++ packages/antd-taro-react/src/TabBar/tab-bar.tsx
@@ -2,45 +2,57 @@ import "./index.scss"
 import React, {FC} from "react";
 import classNames from "classnames";
 
-type TabBarItemProps = {
-  key: React.Key
+type TabBarItemType = {
+  key: string
   label?: React.ReactNode
   icon?: React.ReactNode
   activeIcon?: React.ReactNode
   disabled?: boolean
+  active?: boolean
   activeKey?: string
-  onClick?: (key: string) => void
+  onClick?: () => void
 }
+
+type TabBarItemProps = Omit<TabBarItemType, 'key'> & {}
+
 export type TabBarProps = {
-  items: TabBarItemProps[]
+  items: TabBarItemType[]
+  onClick?: (e: { item?: TabBarItemType }) => void
 }
 const tabBarCls = 'triones-tab-bar'
-export const TabBarItem: FC<TabBarItemProps & {itemKey: string}> = ({itemKey,label, icon, activeIcon, activeKey, onClick,...props}) => {
+export const TabBarItem: FC<TabBarItemProps> = ({
+                                                  label,
+                                                  icon,
+                                                  activeIcon,
+                                                  activeKey,
+                                                  active,
+                                                  onClick,
+                                                  ...props
+                                                }) => {
   console.log(props)
-  const active = Boolean(activeKey) && props.key === activeKey
-  console.log(active)
+
   const displayIcon = active ? (activeIcon ?? icon) : icon
   return <div className={classNames(`${tabBarCls}-item`, {
     [`${tabBarCls}-item-active`]: active
-  })} onClick={() => {
-    onClick?.(props.key.toString())
-  }}>
+  })} onClick={() => onClick?.()}>
     <div>{displayIcon}</div>
     <div className={classNames(`${tabBarCls}-item-label`)}>{label}</div>
   </div>
 }
 
 
-export const TabBar: FC<TabBarProps> = ({items}) => {
-  const [innerActiveKey, setInnerActiveKey] = React.useState<string>()
+export const TabBar: FC<TabBarProps> = ({items, onClick}) => {
+  const [innerActiveKey, setInnerActiveKey] = React.useState<string | undefined>()
 
 
   return <div className={classNames(tabBarCls)}>
     <div className={classNames(`${tabBarCls}-wrap`)}>
-      {items.map(item => <TabBarItem {...item} key={item.key} activeKey={innerActiveKey}
-                                     onClick={key => {
-                                       console.log(key)
-                                       setInnerActiveKey(key)
+      {items.map(item => <TabBarItem {...item} activeKey={innerActiveKey} active={item.key === innerActiveKey}
+                                     onClick={() => {
+                                       onClick?.({item})
+                                       setInnerActiveKey(item.key)
+                                       console.log()
+                                       console.log(item)
                                      }}/>)}
     </div>
   </div>
