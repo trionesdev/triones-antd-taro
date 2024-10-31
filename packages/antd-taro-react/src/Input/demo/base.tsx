9450b943538b9c6ff85116e9d625692edb2fbf60
bane.shi <fengxiaotx@163.com>
2024-10-31 22:41:50 +0800

    feat: init

---
+++ packages/antd-taro-react/src/Input/demo/base.tsx
@@ -0,0 +1,10 @@
+import Input from "@trionesdev/antd-taro-react/Input";
+import React from "react";
+
+export default () => {
+  return <div>
+    <div>
+      <Input placeholder="请输入内容"/>
+    </div>
+  </div>
+}
--- packages/antd-taro-react/src/Input/input.tsx
+++ packages/antd-taro-react/src/Input/input.tsx
@@ -1,5 +1,23 @@
-import React from "react"
+import "./index.scss"
+import React, {FC} from "react"
+import classNames from "classnames";
 
-export const Input = () => {
-    return <div>ss</div>
+export type InputProps = {
+  className?: string,
+  style?: React.CSSProperties,
+  placeholder?: string,
+  disabled?: boolean,
+}
+export const Input: FC<InputProps> = ({className, style, placeholder, disabled}) => {
+
+  const cls = 'triones-antm-input';
+  return <div className={classNames(cls, className)} style={style}>
+    <div className={`${cls}-line`}>
+      <div className={`${cls}-content`}>
+        <div className={`${cls}-wrap`}>
+          <input className={`${cls}-input`} placeholder={placeholder}/>
+        </div>
+      </div>
+    </div>
+  </div>
 }
