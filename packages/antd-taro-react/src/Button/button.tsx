003d287bd9b3de5b3927399e3c56b92f7dbdae57
bane.shi <fengxiaotx@163.com>
2024-12-23 14:31:24 +0800

    feat: init

--- packages/antd-taro-react/src/Button/button.tsx
+++ packages/antd-taro-react/src/Button/button.tsx
@@ -1,95 +1,97 @@
 import "./style.scss"
-import React, {FC} from "react";
+import React, {FC, MouseEventHandler} from "react";
 import classNames from "classnames";
 
 type AntButtonProps = {
-    children?: React.ReactNode
-    /**
-     * block 按钮宽高自适应
-     */
-    block?: boolean
-    /**
-     * 颜色
-     */
-    color?: 'default' | 'primary' | 'success' | 'warning' | 'danger'
-    /**
-     * 是否危险按钮，会使用 danger 的颜色
-     */
-    danger?: boolean
-    /**
-     * 是否禁用
-     */
-    disabled?: boolean
-    /**
-     * 幽灵按钮
-     */
-    ghost?: boolean
-    type?: 'primary' | 'dashed' | 'link' | 'text' | 'default'
+  children?: React.ReactNode
+  /**
+   * block 按钮宽高自适应
+   */
+  block?: boolean
+  /**
+   * 颜色
+   */
+  color?: 'default' | 'primary' | 'success' | 'warning' | 'danger'
+  /**
+   * 是否危险按钮，会使用 danger 的颜色
+   */
+  danger?: boolean
+  /**
+   * 是否禁用
+   */
+  disabled?: boolean
+  /**
+   * 幽灵按钮
+   */
+  ghost?: boolean
+  type?: 'primary' | 'dashed' | 'link' | 'text' | 'default'
 
-    className?: string,
-    style?: React.CSSProperties
-    icon?: React.ReactNode
-    iconPosition?: 'start' | 'end'
-    loading?: boolean
-    variant?: 'outlined' | 'dashed' | 'solid' | 'filled' | 'text' | 'link',
-    onClick?: (event: Event) => void
+  className?: string,
+  style?: React.CSSProperties
+  icon?: React.ReactNode
+  iconPosition?: 'start' | 'end'
+  loading?: boolean
+  variant?: 'outlined' | 'dashed' | 'solid' | 'filled' | 'text' | 'link',
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
 
 export const Button: FC<AntButtonProps> = (
-    {
-        block = false,
-        type = 'default',
-        color,
-        danger = false,
-        disabled,
-        className,
-        style,
-        icon,
-        iconPosition = 'start',
-        loading,
-        variant,
-        ...props
-    }
+  {
+    children,
+    block = false,
+    type = 'default',
+    color,
+    danger = false,
+    disabled,
+    className,
+    style,
+    icon,
+    iconPosition = 'start',
+    loading,
+    variant,
+    onClick,
+    ...props
+  }
 ) => {
-    const clsPrefix = "triones-antm-button"
-    let finalColor = color
-    let finalVariant = variant
-    if (type) {
-        if (!color) {
-            if (type === 'primary' || type === 'link') {
-                finalColor = 'primary'
-            } else if (type === 'default') {
-                finalColor = 'default'
-            }
-        }
-        if (!variant) {
-            if (type === 'primary') {
-                finalVariant = 'solid'
-            } else if (type === 'default') {
-                finalVariant = 'outlined'
-            } else if (type === 'dashed' || type === 'link' || type === 'text') {
-                finalVariant = type
-            }
-        }
-
+  const clsPrefix = "triones-antm-button"
+  let finalColor = color
+  let finalVariant = variant
+  if (type) {
+    if (!color) {
+      if (type === 'primary' || type === 'link') {
+        finalColor = 'primary'
+      } else if (type === 'default') {
+        finalColor = 'default'
+      }
     }
-    if (danger) {
-        finalColor = 'danger'
+    if (!variant) {
+      if (type === 'primary') {
+        finalVariant = 'solid'
+      } else if (type === 'default') {
+        finalVariant = 'outlined'
+      } else if (type === 'dashed' || type === 'link' || type === 'text') {
+        finalVariant = type
+      }
     }
-    return <button
-        {...props}
-        className={classNames(clsPrefix,
-            {
-                [`${clsPrefix}-block`]: block,
-                [`${clsPrefix}-type-${type}`]: type,
-                [`${clsPrefix}-color-${finalColor}`]: finalColor,
-                [`${clsPrefix}-variant-${finalVariant}`]: finalVariant,
 
-            })}
-        disabled={disabled}>
-        {iconPosition == 'start' && icon}
-        {props.children}
-        {iconPosition == 'end' && icon}
-    </button>
+  }
+  if (danger) {
+    finalColor = 'danger'
+  }
+  return <button
+    onClick={onClick}
+    className={classNames(clsPrefix,
+      {
+        [`${clsPrefix}-block`]: block,
+        [`${clsPrefix}-type-${type}`]: type,
+        [`${clsPrefix}-color-${finalColor}`]: finalColor,
+        [`${clsPrefix}-variant-${finalVariant}`]: finalVariant,
+
+      })}
+    disabled={disabled}>
+    {iconPosition == 'start' && icon}
+    {children}
+    {iconPosition == 'end' && icon}
+  </button>
 }
--- packages/antd-taro-react/src/Popover/index.tsx
+++ packages/antd-taro-react/src/Popover/index.tsx
@@ -1,16 +1,2 @@
-import './style.scss';
-import * as React from 'react';
-
-import Tooltip from './tooltip';
-
-function Popover({
-  children,
-  ...restProps
-}) {
-  return <Tooltip {...restProps}>
-    {children}
-  </Tooltip>
-}
-
-type CompoundedComponent = typeof Popover
-export default Popover as CompoundedComponent;
+import {Popover} from "./popover"
+export default Popover;
