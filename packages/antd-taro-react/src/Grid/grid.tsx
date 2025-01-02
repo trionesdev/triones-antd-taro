3d9bef64ee373eba3dc93a034c0f5e11de1928a8
dengyongqiang <dengyongqiang@yonyou.com>
2025-01-02 14:04:52 +0800

    feat: Grid组件开发

---
+++ packages/antd-taro-react/src/Grid/grid.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import type { FC, ReactNode, CSSProperties } from 'react'
+import { mergeProps, NativeProps, toCSSLength, withNativeProps } from '../utils/native-props'
+
+import './style.scss'
+
+const classPrefix = `triones-antm-grid`
+
+export type GridProps = {
+  columns: number
+  gap?: number | string | [number | string, number | string]
+  children?: ReactNode
+} & NativeProps<'--gap' | '--gap-vertical' | '--gap-horizontal'>
+
+export const Grid: FC<GridProps> = props => {
+  const style: GridProps['style'] & Record<'--columns', string> = {
+    '--columns': props.columns.toString(),
+  }
+  const { gap } = props
+  if (gap !== undefined) {
+    if (Array.isArray(gap)) {
+      style['--gap-horizontal'] = toCSSLength(gap[0])
+      style['--gap-vertical'] = toCSSLength(gap[1])
+    } else {
+      style['--gap'] = toCSSLength(gap)
+    }
+  }
+
+  return withNativeProps(
+    props,
+    <div className={classPrefix} style={style}>
+      {props.children}
+    </div>
+  )
+}
+
+export type GridItemProps = {
+  span?: number
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  children?: ReactNode
+} & NativeProps
+
+type GridItemStyle = CSSProperties &
+  Record<'--item-span', GridItemProps['span']>
+
+export const GridItem: FC<GridItemProps> = p => {
+  const props = mergeProps({ span: 1 }, p) as GridItemProps
+
+  const itemStyle: GridItemStyle = {
+    '--item-span': props.span,
+  }
+  return withNativeProps(
+    props,
+    <div
+      className={`${classPrefix}-item`}
+      style={itemStyle}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </div>
+  )
+}
--- packages/antd-taro-react/src/utils/native-props.ts
+++ packages/antd-taro-react/src/utils/native-props.ts
@@ -66,3 +66,8 @@ export function attachPropertiesToComponent<C, P extends Record<string, any>>(
   }
   return ret
 }
+
+
+export function toCSSLength(val: string | number) {
+  return typeof val === 'number' ? `${val}px` : val
+}
