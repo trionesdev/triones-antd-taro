1f65fc7f0d0762e0149188dd39b3e01473268e2e
落雁沙 <fengxiaotx@163.com>
2025-01-07 16:50:56 +0800

    feat:

--- packages/antd-taro-react/src/Input/input.tsx
+++ packages/antd-taro-react/src/Input/input.tsx
@@ -1,38 +1,63 @@
-import "../style/asset.scss"
-import "./index.scss"
-import React, {FC} from "react"
-import classNames from "classnames";
+import classNames from 'classnames';
+import React, { FC } from 'react';
+import '../style/asset.scss';
+import './index.scss';
 
 export type InputProps = {
-  className?: string,
-  style?: React.CSSProperties,
-  placeholder?: string,
-  disabled?: boolean,
-  allowClear?: boolean,
-  prefix?: React.ReactNode,
-  suffix?: React.ReactNode,
-  value?:any,
-  onChange?:(e:any)=>void
-}
-export const Input: FC<InputProps> = ({className, style, placeholder, disabled, allowClear, prefix, suffix,value,onChange}) => {
-  const [innerValue, setInnerValue] = React.useState(value)
+  className?: string;
+  style?: React.CSSProperties;
+  placeholder?: string;
+  disabled?: boolean;
+  allowClear?: boolean;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  value?: any;
+  onChange?: (e: any) => void;
+};
+export const Input: FC<InputProps> = ({
+  className,
+  style,
+  placeholder,
+  disabled,
+  allowClear,
+  prefix,
+  suffix,
+  value,
+  onChange,
+}) => {
+  const [innerValue, setInnerValue] = React.useState(value);
   const cls = 'triones-antm-input';
-  return <div className={classNames(cls, className)} style={style}>
-    <div className={`${cls}-line`}>
-
-      <div className={`${cls}-content`}>
-        <div className={`${cls}-wrap`}>
-          <input className={`${cls}-input`} placeholder={placeholder} value={innerValue} onChange={(e)=>{
-            setInnerValue(e.target.value)
-            onChange?.(e)
-          }}/>
+  return (
+    <div className={classNames(cls, className)} style={style}>
+      <div className={`${cls}-line`}>
+        <div className={`${cls}-content`}>
+          {prefix && <div className={`${cls}-prefix`}>{prefix}</div>}
+          <div className={`${cls}-wrap`}>
+            <input
+              className={`${cls}-input`}
+              placeholder={placeholder}
+              value={innerValue}
+              onChange={(e) => {
+                setInnerValue(e.target.value);
+                onChange?.(e);
+              }}
+            />
+          </div>
+          {(suffix || allowClear) && (
+            <div className={`${cls}-suffix`}>
+              {allowClear && innerValue && (
+                <i
+                  className={`clear-icon`}
+                  onClick={() => {
+                    setInnerValue('');
+                  }}
+                />
+              )}
+              {suffix}
+            </div>
+          )}
         </div>
-        {(prefix || allowClear) && <div className={`${cls}-suffix`}>
-          {allowClear && innerValue && <i className={`clear-icon`} onClick={()=>{
-            setInnerValue('')
-          }}/>}
-        </div>}
       </div>
     </div>
-  </div>
-}
+  );
+};
