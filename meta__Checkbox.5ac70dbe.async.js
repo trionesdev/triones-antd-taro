dcd23d1068b494214b748d77efec56e2b7c19347
fengxiaotx <fengxiaotx@users.noreply.github.com>
2025-01-12 14:06:03 +0000

    deploy: c71d687886b4b2766a6b00443016f18890625eb1

---
+++ meta__Checkbox.5ac70dbe.async.js
@@ -0,0 +1,38 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[4059],{72655:function(c,n,e){var r;e.r(n),e.d(n,{demos:function(){return v}});var s=e(90819),d=e.n(s),p=e(89933),i=e.n(p),o=e(44194),b=e(67711),h=e(76282),k=e(63457),v={"checkbox-demo-base":{component:o.memo(o.lazy(function(){return e.e(9709).then(e.bind(e,59921))})),asset:{type:"BLOCK",id:"checkbox-demo-base",refAtomIds:["Checkbox"],dependencies:{"index.tsx":{type:"FILE",value:e(7e3).Z},"@trionesdev/antd-taro-react":{type:"NPM",value:"0.0.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-taro-react":h,"@trionesdev/antd-taro-react/DemoBlock":k,react:r||(r=e.t(o,2))},renderOpts:{compile:function(){var l=i()(d()().mark(function m(){var a,u=arguments;return d()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(3261).then(e.bind(e,13261));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},m)}));function _(){return l.apply(this,arguments)}return _}()}},"checkbox-demo-disabled":{component:o.memo(o.lazy(function(){return e.e(9709).then(e.bind(e,98702))})),asset:{type:"BLOCK",id:"checkbox-demo-disabled",refAtomIds:["Checkbox"],dependencies:{"index.tsx":{type:"FILE",value:e(80959).Z},"@trionesdev/antd-taro-react":{type:"NPM",value:"0.0.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx"},context:{"@trionesdev/antd-taro-react":h,"@trionesdev/antd-taro-react/DemoBlock":k,react:r||(r=e.t(o,2))},renderOpts:{compile:function(){var l=i()(d()().mark(function m(){var a,u=arguments;return d()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,e.e(3261).then(e.bind(e,13261));case 2:return t.abrupt("return",(a=t.sent).default.apply(a,u));case 3:case"end":return t.stop()}},m)}));function _(){return l.apply(this,arguments)}return _}()}}}},73319:function(c,n,e){e.r(n),e.d(n,{texts:function(){return s}});var r=e(67711);const s=[]},7e3:function(c,n){n.Z=`import { Checkbox } from "@trionesdev/antd-taro-react";
+import { DemoBlock } from "@trionesdev/antd-taro-react/DemoBlock";
+import React from "react";
+
+export default () => {
+  return <>
+    <DemoBlock title='\u57FA\u7840\u7528\u6CD5'>
+      <Checkbox checked={true}>\u82F9\u679C</Checkbox>
+      <Checkbox checked={false}>\u9999\u8549</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='\u9ED8\u8BA4\u9009\u4E2D'>
+      <Checkbox checked={true}>\u82F9\u679C</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='\u7981\u7528'>
+      <Checkbox checked={true} disabled={true}>\u82F9\u679C</Checkbox>
+      <Checkbox checked={false} disabled={true}>\u9999\u8549</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='Button'>
+      <Checkbox checked={true} shape={'button'}>\u82F9\u679C</Checkbox>
+    </DemoBlock>
+  </>
+}
+`},80959:function(c,n){n.Z=`import { Checkbox } from "@trionesdev/antd-taro-react";
+import { DemoBlock } from "@trionesdev/antd-taro-react/DemoBlock";
+import React from "react";
+
+export default () => {
+  return <>
+    <DemoBlock title='\u7981\u7528'>
+      <Checkbox checked={true} disabled={true}>\u82F9\u679C</Checkbox>
+      <Checkbox checked={false} disabled={true}>\u9999\u8549</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='Button'>
+      <Checkbox checked={true} shape={'button'} disabled={true}>\u82F9\u679C</Checkbox>
+    </DemoBlock>
+  </>
+}
+`}}]);
---
+++ meta__Icon.ac3a5a96.async.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[2031],{68738:function(_,t,n){var o;n.r(t),n.d(t,{demos:function(){return v}});var a=n(90819),s=n.n(a),l=n(89933),i=n.n(l),r=n(44194),f=n(70134),c=n(84474),d=n(39378),M=n.n(d),u=n(63457),v={"src-icon-demo-base":{component:r.memo(r.lazy(function(){return Promise.all([n.e(9378),n.e(6282),n.e(2433)]).then(n.bind(n,22982))})),asset:{type:"BLOCK",id:"src-icon-demo-base",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(50890).Z},react:{type:"NPM",value:"18.3.1"},"@trionesdev/antd-taro-icons-react":{type:"NPM",value:"0.0.1"},lodash:{type:"NPM",value:"4.17.21"},"@trionesdev/antd-taro-react":{type:"NPM",value:"0.0.0"}},entry:"index.tsx"},context:{react:o||(o=n.t(r,2)),"@trionesdev/antd-taro-icons-react":c,lodash:d,"@trionesdev/antd-taro-react/DemoBlock":u},renderOpts:{compile:function(){var E=i()(s()().mark(function p(){var m,P=arguments;return s()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(3261).then(n.bind(n,13261));case 2:return e.abrupt("return",(m=e.sent).default.apply(m,P));case 3:case"end":return e.stop()}},p)}));function D(){return E.apply(this,arguments)}return D}()}}}},61868:function(_,t,n){n.r(t),n.d(t,{texts:function(){return a}});var o=n(70134);const a=[]},50890:function(_,t){t.Z=`import React from "react";
+import * as icons from "@trionesdev/antd-taro-icons-react"
+import _ from "lodash";
+import { DemoBlock } from "@trionesdev/antd-taro-react/DemoBlock";
+
+export default () => {
+  return <DemoBlock title='\u56FE\u6807'>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 20, flexWrap: 'wrap' }}>
+      {
+        _.map(_.omit(icons, 'Icon'), (icon, name) => {
+          return <div key={name} style={{ width: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 30 }}>
+            <div>{React.createElement(icon, { key: name })}</div>
+            <div style={{ fontSize: 12 }}>{name}</div>
+          </div>
+        })
+      }
+    </div>
+  </DemoBlock >
+}
+`}}]);
