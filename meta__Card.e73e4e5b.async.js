463c1be46c6bf8d15824a3b170e5081351865c15
fengxiaotx <fengxiaotx@users.noreply.github.com>
2025-01-12 08:17:31 +0000

    deploy: c86cf07a40a1360a2201e84c65e386c24a7880e5

---
+++ meta__Card.e73e4e5b.async.js
@@ -0,0 +1,80 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[9477],{43015:function(i,n,a){var I;a.r(n),a.d(n,{demos:function(){return r}});var d=a(90819),u=a.n(d),x=a(89933),l=a.n(x),m=a(44194),s=a(77817),c=a(54395),p=a(63457),_=a(51865),t=a.n(_),e=a(64743),r={"card-demo-base":{component:m.memo(m.lazy(function(){return a.e(1058).then(a.bind(a,41682))})),asset:{type:"BLOCK",id:"card-demo-base",refAtomIds:["Card"],dependencies:{"index.tsx":{type:"FILE",value:a(78246).Z},"@trionesdev/antd-taro-react":{type:"NPM",value:"0.0.0"},classnames:{type:"NPM",value:"2.5.1"},react:{type:"NPM",value:"18.3.1"},"./base.scss":{type:"FILE",value:a(79715).Z}},entry:"index.tsx"},context:{"./base.scss":e,"@trionesdev/antd-taro-react/Card":c,"@trionesdev/antd-taro-react/DemoBlock":p,classnames:_,react:I||(I=a.t(m,2)),"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Card/demo/base.scss":e},renderOpts:{compile:function(){var o=l()(u()().mark(function f(){var C,E=arguments;return u()().wrap(function(v){for(;;)switch(v.prev=v.next){case 0:return v.next=2,a.e(8863).then(a.bind(a,68863));case 2:return v.abrupt("return",(C=v.sent).default.apply(C,E));case 3:case"end":return v.stop()}},f)}));function h(){return o.apply(this,arguments)}return h}()}}}},54395:function(i,n,a){a.r(n),a.d(n,{default:function(){return _}});var I=a(51865),d=a.n(I),u=a(44194),x=a(73193),l=a.n(x);function m(t,e){var r=l()({},e.props);t.className&&(r.className=d()(e.props.className,t.className)),t.style&&(r.style=l()(l()({},r.style),t.style)),t.tabIndex!==void 0&&(r.tabIndex=t.tabIndex);for(var o in t)t.hasOwnProperty(o)&&(o.startsWith("data-")||o.startsWith("aria-"))&&(r[o]=t[o]);return u.cloneElement(e,r)}var s=a(31549),c="triones-antm-card",p=function(e){var r=function(){return e.title||e.extra?(0,s.jsxs)("div",{className:d()("".concat(c,"-header"),e.headerClassName),style:e.headerStyle,onClick:e.onHeaderClick,children:[e.icon&&(0,s.jsx)("div",{className:"".concat(c,"-header-icon"),children:e.icon}),(0,s.jsx)("div",{className:"".concat(c,"-header-title"),children:e.title}),e.extra&&(0,s.jsx)("div",{className:"".concat(c,"-header-extra"),children:e.extra})]}):null},o=function(){return e.children?(0,s.jsx)("div",{className:d()("".concat(c,"-body"),e.bodyClassName),style:e.bodyStyle,onClick:e.onBodyClick,children:e.children}):null};return m(e,(0,s.jsxs)("div",{className:c,onClick:e.onClick,children:[r(),o()]}))},_=p},63457:function(i,n,a){a.r(n),a.d(n,{DemoBlock:function(){return u}});var I=a(44194),d=a(31549),u=function(l){return(0,d.jsxs)("div",{className:"demoBlock",children:[(0,d.jsx)("div",{className:"demoTitle",children:l.title}),(0,d.jsx)("div",{className:"demoMain",style:{padding:"12px 12px",background:"#ffffff"},children:l.children})]})}},64743:function(i,n,a){a.r(n)},91567:function(i,n,a){a.r(n),a.d(n,{texts:function(){return d}});var I=a(77817);const d=[{value:"\u901A\u7528\u5361\u7247\u5BB9\u5668\u3002",paraId:0,tocIndex:0},{value:"\u53EF\u627F\u8F7D\u6587\u5B57\u3001\u5217\u8868\u3001\u56FE\u7247\u3001\u6BB5\u843D\u7B49\uFF0C\u4FBF\u4E8E\u7528\u6237\u6D4F\u89C8\u5185\u5BB9\u3002",paraId:1,tocIndex:1},{value:"\u5C5E\u6027",paraId:2,tocIndex:4},{value:"\u8BF4\u660E",paraId:2,tocIndex:4},{value:"\u7C7B\u578B",paraId:2,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:4},{value:"\u7248\u672C",paraId:2,tocIndex:4},{value:"bodyClassName",paraId:2,tocIndex:4},{value:"body \u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"bodyStyle",paraId:2,tocIndex:4},{value:"body \u81EA\u5B9A\u4E49\u6837\u5F0F",paraId:2,tocIndex:4},{value:"React.CSSProperties",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"extra",paraId:2,tocIndex:4},{value:"header \u53F3\u8FB9\u533A\u57DF",paraId:2,tocIndex:4},{value:"ReactNode",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"headerClassName",paraId:2,tocIndex:4},{value:"header \u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"headerStyle",paraId:2,tocIndex:4},{value:"header \u81EA\u5B9A\u4E49\u6837\u5F0F",paraId:2,tocIndex:4},{value:"React.CSSProperties",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"icon",paraId:2,tocIndex:4},{value:"header \u5DE6\u8FB9\u56FE\u6807",paraId:2,tocIndex:4},{value:"ReactNode",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"5.38.0",paraId:2,tocIndex:4},{value:"onBodyClick",paraId:2,tocIndex:4},{value:"body \u533A\u57DF\u70B9\u51FB\u4E8B\u4EF6",paraId:2,tocIndex:4},{value:"(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"onClick",paraId:2,tocIndex:4},{value:"\u5361\u7247\u70B9\u51FB\u4E8B\u4EF6",paraId:2,tocIndex:4},{value:"(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"onHeaderClick",paraId:2,tocIndex:4},{value:"header \u533A\u57DF\u70B9\u51FB\u4E8B\u4EF6",paraId:2,tocIndex:4},{value:"(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"title",paraId:2,tocIndex:4},{value:"header \u5DE6\u8FB9\u533A\u57DF",paraId:2,tocIndex:4},{value:"ReactNode",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"\u5C5E\u6027",paraId:3,tocIndex:5},{value:"\u8BF4\u660E",paraId:3,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:5},{value:"\u5168\u5C40\u53D8\u91CF",paraId:3,tocIndex:5},{value:"\u7248\u672C",paraId:3,tocIndex:5},{value:"--triones-antm-card-border-radius",paraId:3,tocIndex:5},{value:"\u5706\u89D2\u5927\u5C0F",paraId:3,tocIndex:5},{value:"8px",paraId:3,tocIndex:5},{value:"--triones-antm-card-border-radius",paraId:3,tocIndex:5},{value:"--triones-antm-card-padding-inline",paraId:3,tocIndex:5},{value:"\u6C34\u5E73\u5185\u8FB9\u8DDD",paraId:3,tocIndex:5},{value:"12px",paraId:3,tocIndex:5},{value:"--triones-antm-card-padding-inline",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-border-width",paraId:3,tocIndex:5},{value:"header \u5206\u5272\u7EBF\u5BBD\u5EA6",paraId:3,tocIndex:5},{value:"0.5px",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-border-width",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-border-color",paraId:3,tocIndex:5},{value:"header \u5206\u5272\u7EBF\u989C\u8272",paraId:3,tocIndex:5},{value:"var(--triones-antm-color-border)",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-border-color",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-gap",paraId:3,tocIndex:5},{value:"header \u6C34\u5E73\u95F4\u9699",paraId:3,tocIndex:5},{value:"8px",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-gap",paraId:3,tocIndex:5},{value:"5.38.0",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-padding-block",paraId:3,tocIndex:5},{value:"header \u5782\u76F4\u5185\u8FB9\u8DDD",paraId:3,tocIndex:5},{value:"12px",paraId:3,tocIndex:5},{value:"--triones-antm-card-header-padding-block",paraId:3,tocIndex:5},{value:"--triones-antm-card-body-padding-block",paraId:3,tocIndex:5},{value:"body \u5782\u76F4\u5185\u8FB9\u8DDD",paraId:3,tocIndex:5},{value:"12px",paraId:3,tocIndex:5},{value:"--triones-antm-card-body-padding-block",paraId:3,tocIndex:5}]},79715:function(i,n){n.Z=`$class-prefix-card: 'card-demo';
+
+.#{$class-prefix-card} {
+  &-content {
+    height: 50px;
+  }
+  &-footer {
+    padding-top: 11px;
+    border-top: 1px solid #e5e5e5;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  &-customBody {
+    color: green;
+  }
+
+}
+`},78246:function(i,n){n.Z=`import Card from '@trionesdev/antd-taro-react/Card';
+import { DemoBlock } from '@trionesdev/antd-taro-react/DemoBlock';
+import classNames from 'classnames';
+import React from 'react';
+
+import './base.scss';
+const classPrefix = \`card-demo\`;
+
+export default () => {
+  const onClick = () => {
+    console.log('\u70B9\u51FB\u4E86\u5361\u7247');
+  };
+
+  const onHeaderClick = () => {
+    console.log('\u70B9\u51FB\u4E86\u5361\u7247Header\u533A\u57DF');
+  };
+
+  const onBodyClick = () => {
+    console.log('\u70B9\u51FB\u4E86\u5361\u7247Body\u533A\u57DF');
+  };
+  return (
+    <>
+      <DemoBlock title="\u57FA\u7840\u7528\u6CD5">
+        <Card title="\u5361\u7247\u6807\u9898" onClick={onClick}>
+          \u5361\u7247\u5185\u5BB9
+        </Card>
+      </DemoBlock>
+      <DemoBlock title="\u6CA1\u6709\u5361\u7247\u5185\u5BB9">
+        <Card title="\u5361\u7247\u6807\u9898" onClick={onClick} />
+      </DemoBlock>
+      <DemoBlock title="\u6CA1\u6709\u5361\u7247\u6807\u9898">
+        <Card onClick={onClick}>\u5361\u7247\u5185\u5BB9</Card>
+      </DemoBlock>
+      <DemoBlock title="\u81EA\u5B9A\u4E49\u5361\u7247\u5185\u5BB9">
+        <Card
+          title={<div style={{ fontWeight: 'normal' }}>\u5361\u7247\u6807\u9898</div>}
+          onBodyClick={onBodyClick}
+          onHeaderClick={onHeaderClick}
+          style={{ borderRadius: '16px' }}
+        >
+          <div className={classNames(\`\${classPrefix}-content\`)}>\u5361\u7247\u5185\u5BB9</div>
+          <div
+            className={classNames(\`\${classPrefix}-footer\`)}
+            onClick={(e) => e.stopPropagation()}
+          ></div>
+        </Card>
+      </DemoBlock>
+      <DemoBlock title="\u81EA\u5B9A\u4E49\u5361\u7247\u6837\u5F0F">
+        <Card
+          headerStyle={{
+            color: '#1677ff',
+          }}
+          bodyClassName={classNames(\`\${classPrefix}-customBody\`)}
+          title="\u5361\u7247\u6807\u9898"
+        >
+          \u5361\u7247\u5185\u5BB9
+        </Card>
+      </DemoBlock>
+    </>
+  );
+};
+`}}]);
