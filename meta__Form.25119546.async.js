4628bc354059823a0ab57f5b580b037e6930a023
fengxiaotx <fengxiaotx@users.noreply.github.com>
2025-01-25 15:22:43 +0000

    deploy: e4c5b8705da1c47dbf11d9ec11e3ca9831202588

---
+++ meta__Form.25119546.async.js
@@ -0,0 +1,111 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[252],{50533:function(r,n,t){var _;t.r(n),t.d(n,{demos:function(){return K}});var s=t(90819),o=t.n(s),d=t(89933),m=t.n(d),a=t(44194),j=t(97202),i=t(3405),l=t(93977),c=t(49387),u=t(16379),p=t(37180),E=t(33550),x=t(63346),I=t(95611),f=t(55888),h=t(69295),v=t(95587),F=t(53974),T=t(27311),D=t(51865),$=t.n(D),A=t(68161),M=t(24701),P=t(71318),O=t(23421),g=t(21497),B=t(85279),k=t(95432),L=t(61816),y=t(11421),C=t(39378),z=t.n(C),K={"form-demo-base":{component:a.memo(a.lazy(function(){return t.e(5943).then(t.bind(t,47889))})),asset:{type:"BLOCK",id:"form-demo-base",refAtomIds:["Form"],dependencies:{"index.tsx":{type:"FILE",value:t(24167).Z},react:{type:"NPM",value:"18.3.1"},"../index.tsx":{type:"FILE",value:t(10927).Z},"../../Button.tsx":{type:"FILE",value:t(960).Z},"../../DemoBlock.tsx":{type:"FILE",value:t(28667).Z},"../../Input.tsx":{type:"FILE",value:t(68217).Z},"./button.tsx":{type:"FILE",value:t(94900).Z},"./index.scss":{type:"FILE",value:t(12823).Z},"./input-opt.tsx":{type:"FILE",value:t(1039).Z},"./hooks/useForm.ts":{type:"FILE",value:t(44010).Z},"./input-password.tsx":{type:"FILE",value:t(71558).Z},"./form.tsx":{type:"FILE",value:t(52440).Z},"./input.tsx":{type:"FILE",value:t(6870).Z},"./FormItem.tsx":{type:"FILE",value:t(13137).Z},"@trionesdev/antd-taro-icons-react":{type:"NPM",value:"0.0.1"},classnames:{type:"NPM",value:"2.5.1"},"rc-field-form":{type:"NPM",value:"2.7.0"},"./style.scss":{type:"FILE",value:t(36116).Z},"./form-item.tsx":{type:"FILE",value:t(26679).Z},"./context.tsx":{type:"FILE",value:t(25628).Z},"./input-affix-wrapper.tsx":{type:"FILE",value:t(10793).Z},"./base-input.tsx":{type:"FILE",value:t(83165).Z},"../hooks/useFormContext.tsx":{type:"FILE",value:t(89336).Z},"./form-item-input.tsx":{type:"FILE",value:t(57426).Z},"./form-item-label.tsx":{type:"FILE",value:t(73951).Z},lodash:{type:"NPM",value:"4.17.21"}},entry:"index.tsx"},context:{"../index.tsx":i,"../../Button.tsx":l,"../../DemoBlock.tsx":c,"../../Input.tsx":u,"./button.tsx":p,"./index.scss":E,"./input-opt.tsx":x,"./hooks/useForm.ts":I,"./input-password.tsx":f,"./form.tsx":h,"./input.tsx":v,"./FormItem.tsx":F,"./style.scss":M,"./form-item.tsx":P,"./context.tsx":O,"./input-affix-wrapper.tsx":g,"./base-input.tsx":B,"../hooks/useFormContext.tsx":k,"./form-item-input.tsx":L,"./form-item-label.tsx":y,react:_||(_=t.t(a,2)),"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/index.tsx":i,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Button/index.tsx":l,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.tsx":c,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/index.tsx":u,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Button/button.tsx":p,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/index.scss":E,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/input-opt.tsx":x,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/hooks/useForm.ts":I,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/input-password.tsx":f,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/form.tsx":h,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/input.tsx":v,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/FormItem/index.tsx":F,"@trionesdev/antd-taro-icons-react":T,classnames:D,"rc-field-form":A,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/style.scss":M,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/FormItem/form-item.tsx":P,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/context.tsx":O,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/input-affix-wrapper.tsx":g,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Input/base-input.tsx":B,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/hooks/useFormContext.tsx":k,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/FormItem/form-item-input.tsx":L,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Form/FormItem/form-item-label.tsx":y,lodash:C},renderOpts:{compile:function(){var W=m()(o()().mark(function Z(){var R,N=arguments;return o()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,t.e(6566).then(t.bind(t,66566));case 2:return e.abrupt("return",(R=e.sent).default.apply(R,N));case 3:case"end":return e.stop()}},Z)}));function U(){return W.apply(this,arguments)}return U}()}}}},49387:function(r,n,t){t.r(n),t.d(n,{DemoBlock:function(){return d}});var _=t(44194),s=t(91518),o=t(31549),d=function(a){return(0,o.jsxs)("div",{className:"demoBlock",children:[(0,o.jsx)("div",{className:"demoTitle",children:a.title}),(0,o.jsx)("div",{className:"demoMain",style:{padding:"12px 12px",background:"#ffffff"},children:a.children})]})}},91518:function(r,n,t){t.r(n)},9900:function(r,n,t){t.r(n),t.d(n,{texts:function(){return s}});var _=t(97202);const s=[]},28667:function(r,n){n.Z=`import React from 'react'
+import type { FC, ReactNode } from 'react'
+import './index.scss'
+
+interface Props {
+  title: string
+  padding?: string
+  background?: string
+  children?: ReactNode
+}
+
+export const DemoBlock: FC<Props> = props => {
+  return (
+    <div className='demoBlock'>
+      <div className='demoTitle'>{props.title}</div>
+      <div
+        className='demoMain'
+        style={{
+          padding: '12px 12px',
+          background: '#ffffff',
+        }}
+      >
+        {props.children}
+      </div>
+    </div>
+  )
+}
+`},24167:function(r,n){n.Z=`import {DemoBlock} from '../../DemoBlock';
+import React from "react"
+import Form from "../index";
+import Input from "../../Input";
+import Button from "../../Button";
+
+export default () => {
+  const [verticalForm] = Form.useForm();
+  const [horizontalForm] = Form.useForm();
+  return <div>
+    <DemoBlock title='\u6C34\u5E73\u8868\u5355'>
+      <Form form={verticalForm} labelWidth={80}>
+        <Form.Item label={'\u59D3\u540D'} name={'name'} required={true} rules={[{required: true, message: '\u8BF7\u8F93\u5165\u59D3\u540D'}]}>
+          <Input placeholder={\`\u8BF7\u8F93\u5165\u59D3\u540D\`}/>
+        </Form.Item>
+        <Form.Item label={'\u5730\u5740'} name={'address'} required={true} rules={[{required: true, message: '\u8BF7\u8F93\u5165\u5730\u5740'}]}>
+          <Input placeholder={\`\u8BF7\u8F93\u5165\u5730\u5740\`}/>
+        </Form.Item>
+      </Form>
+      <Button type={\`primary\`} block={true} onClick={() => {
+        verticalForm.validateFields().then(values => {
+          console.log(values)
+        })
+      }}>\u63D0\u4EA4</Button>
+    </DemoBlock>
+    <DemoBlock title='\u5782\u76F4\u8868\u5355'>
+      <Form layout={\`vertical\`} form={horizontalForm}>
+        <Form.Item label={'\u59D3\u540D'} name={'name'}>
+          <Input placeholder={\`\u8BF7\u8F93\u5165\u59D3\u540D\`}/>
+        </Form.Item>
+        <Form.Item label={'\u5730\u5740'} name={'address'}>
+          <Input placeholder={\`\u8BF7\u8F93\u5165\u5730\u5740\`}/>
+        </Form.Item>
+      </Form>
+      <Button type={\`primary\`} block={true} onClick={() => {
+        horizontalForm.validateFields().then(values => {
+          console.log(values)
+        })
+      }}>\u63D0\u4EA4</Button>
+    </DemoBlock>
+  </div>
+}
+`},36116:function(r,n){n.Z=`$cls-prefix-form: 'triones-antm-form';
+$cls-prefix-form-item: 'triones-antm-form-item';
+
+.#{$cls-prefix-form}{
+
+}
+
+.#{$cls-prefix-form-item}{
+  &-label{
+    line-height: 1.5;
+    color: #6F6F6F;
+    &-left{}
+    &-right{
+      text-align: right;
+    }
+    &-required-mark{
+      display: inline-block;
+      color: red;
+      margin-right: 4px;
+    }
+  }
+  &-input{
+    .#{$cls-prefix-form-item}{
+      &-error{
+        font-size: 12px;
+        color: red;
+      }
+    }
+  }
+  &-horizontal{
+    display: flex;
+    .#{$cls-prefix-form-item}{
+      &-label{
+        padding: 4px;
+      }
+      &-input{
+        flex: auto 1;
+      }
+    }
+  }
+}
+`}}]);
