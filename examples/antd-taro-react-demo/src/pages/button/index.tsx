1a8caccf1c4756ca2b9181426db4d7a6f72f8f19
落雁沙 <fengxiaotx@163.com>
2025-01-25 23:19:59 +0800

    feat: bug fix

--- examples/antd-taro-react-demo/src/pages/button/index.tsx
+++ examples/antd-taro-react-demo/src/pages/button/index.tsx
@@ -1,9 +1,16 @@
-import {default as ButtonBaseDemo} from "@trionesdev/antd-taro-react/dist/Button/demo/base";
+import {View} from "@tarojs/components";
+import { Button } from "@trionesdev/antd-taro-react";
 
 const ButtonBase = () => {
-  return <div style={{padding: '8px'}}>
-    <div>按钮类型</div>
-    <ButtonBaseDemo/>
-  </div>
+  return <View style={{padding: '8px'}}>
+    <View>按钮类型</View>
+    <View>
+      <Button type={`primary`}>Primary</Button>
+      <Button type={`default`}>Default</Button>
+      <Button type={`dashed`}>Dashed</Button>
+      <Button type={`text`}>Text</Button>
+      <Button type={`link`}>Link</Button>
+    </View>
+  </View>
 }
 export default ButtonBase
