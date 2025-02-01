6823798c42e6542d9612874761bc94d7ec0a117f
fengxiaotx <fengxiaotx@users.noreply.github.com>
2025-02-01 07:57:55 +0000

    deploy: 010885ac2ec756d670530367099a6b415a73348e

---
+++ meta__Badge.979382b8.async.js
@@ -0,0 +1,300 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[2887],{54391:function(a,e,n){var l;n.r(e),n.d(e,{demos:function(){return f}});var o=n(90819),_=n.n(o),m=n(89933),v=n.n(m),c=n(44194),P=n(57058),p=n(76373),t=n(10573),s=n(49387),r=n(6769),d=n(57858),i=n(91518),u=n(51865),B=n.n(u),x=n(45529),f={"src-badge-demo-base":{component:c.memo(c.lazy(function(){return Promise.all([n.e(3849),n.e(9378),n.e(1588),n.e(7311),n.e(1715),n.e(9985),n.e(9842),n.e(2433)]).then(n.bind(n,23844))})),asset:{type:"BLOCK",id:"src-badge-demo-base",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:n(28395).Z},react:{type:"NPM",value:"18.3.1"},"../index.ts":{type:"FILE",value:n(29871).Z},"./base.scss":{type:"FILE",value:n(76977).Z},"../../DemoBlock.tsx":{type:"FILE",value:n(7761).Z},"./badge.tsx":{type:"FILE",value:n(37814).Z},"../utils/native-props.ts":{type:"FILE",value:n(12636).Z},"./index.scss":{type:"FILE",value:n(21791).Z},classnames:{type:"NPM",value:"2.5.1"},"./style.scss":{type:"FILE",value:n(50943).Z}},entry:"index.tsx"},context:{"../index.ts":p,"./base.scss":t,"../../DemoBlock.tsx":s,"./badge.tsx":r,"../utils/native-props.ts":d,"./index.scss":i,"./style.scss":x,react:l||(l=n.t(c,2)),"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Badge/index.ts":p,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Badge/demo/base.scss":t,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.tsx":s,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Badge/badge.tsx":r,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/utils/native-props.ts":d,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.scss":i,classnames:u,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Badge/style.scss":x},renderOpts:{compile:function(){var E=v()(_()().mark(function D(){var h,b=arguments;return _()().wrap(function(I){for(;;)switch(I.prev=I.next){case 0:return I.next=2,n.e(4521).then(n.bind(n,24521));case 2:return I.abrupt("return",(h=I.sent).default.apply(h,b));case 3:case"end":return I.stop()}},D)}));function g(){return E.apply(this,arguments)}return g}()}}}},6769:function(a,e,n){n.r(e),n.d(e,{Badge:function(){return r},dot:function(){return s}});var l=n(10154),o=n.n(l),_=n(51865),m=n.n(_),v=n(44194),c=n(57858),P=n(45529),p=n(31549),t="triones-antm-badge",s=(0,p.jsx)(v.Fragment,{}),r=function(i){var u=i.content,B=i.color,x=i.children,f=u===s,E=m()(t,o()(o()(o()({},"".concat(t,"-fixed"),!!x),"".concat(t,"-dot"),f),"".concat(t,"-bordered"),i.bordered)),g=u||u===0?(0,c.withNativeProps)(i,(0,p.jsx)("div",{className:E,style:{"--color":B},children:!f&&(0,p.jsx)("div",{className:"".concat(t,"-content"),children:u})})):null;return x?(0,p.jsxs)("div",{className:m()("".concat(t,"-wrapper"),i.wrapperClassName),style:i.wrapperStyle,children:[x,g]}):g}},76373:function(a,e,n){n.r(e);var l=n(57858),o=n(6769);e.default=(0,l.attachPropertiesToComponent)(o.Badge,{dot:o.dot})},49387:function(a,e,n){n.r(e),n.d(e,{DemoBlock:function(){return m}});var l=n(44194),o=n(91518),_=n(31549),m=function(c){return(0,_.jsxs)("div",{className:"demoBlock",children:[(0,_.jsx)("div",{className:"demoTitle",children:c.title}),(0,_.jsx)("div",{className:"demoMain",style:{padding:"12px 12px",background:"#ffffff"},children:c.children})]})}},57858:function(a,e,n){n.r(e),n.d(e,{attachPropertiesToComponent:function(){return P},toCSSLength:function(){return p},withNativeProps:function(){return c}});var l=n(73193),o=n.n(l),_=n(44194),m=n(51865),v=n.n(m);function c(t,s){var r=o()({},s.props);t.className&&(r.className=v()(s.props.className,t.className)),t.style&&(r.style=o()(o()({},r.style),t.style)),t.tabIndex!==void 0&&(r.tabIndex=t.tabIndex);for(var d in t)t.hasOwnProperty(d)&&(d.startsWith("data-")||d.startsWith("aria-"))&&(r[d]=t[d]);return _.cloneElement(s,r)}function P(t,s){var r=t;for(var d in s)s.hasOwnProperty(d)&&(r[d]=s[d]);return r}function p(t){return typeof t=="number"?"".concat(t,"px"):t}},10573:function(a,e,n){n.r(e)},45529:function(a,e,n){n.r(e)},91518:function(a,e,n){n.r(e)},95275:function(a,e,n){n.r(e),n.d(e,{texts:function(){return o}});var l=n(57058);const o=[{value:"\u5728\u53F3\u4E0A\u89D2\u5C55\u793A\u6570\u5B57\u3001\u6587\u5B57\u3001\u5C0F\u7EA2\u70B9\u3002",paraId:0,tocIndex:0},{value:"\u9002\u7528\u4E8E\u4EA7\u54C1\u5316\u7684\u65B0\u6D88\u606F\u3001\u65B0\u529F\u80FD\u3001\u65B0\u670D\u52A1\u7B49\u5185\u5BB9\u7684\u63D0\u9192\uFF0C\u901A\u8FC7\u9192\u76EE\u89C6\u89C9\u5F62\u5F0F\u5438\u5F15\u7528\u6237\u5904\u7406\u3002",paraId:1,tocIndex:1},{value:"\u5C5E\u6027",paraId:2,tocIndex:4},{value:"\u8BF4\u660E",paraId:2,tocIndex:4},{value:"\u7C7B\u578B",paraId:2,tocIndex:4},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:4},{value:"bordered",paraId:2,tocIndex:4},{value:"\u662F\u5426\u589E\u52A0\u63CF\u8FB9",paraId:2,tocIndex:4},{value:"boolean",paraId:2,tocIndex:4},{value:"false",paraId:2,tocIndex:4},{value:"color",paraId:2,tocIndex:4},{value:"\u5FBD\u6807\u80CC\u666F\u8272\uFF0C\u7B49\u6548\u4E8E\u8BBE\u7F6E ",paraId:2,tocIndex:4},{value:"--color",paraId:2,tocIndex:4},{value:" CSS \u53D8\u91CF",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"content",paraId:2,tocIndex:4},{value:"\u5FBD\u6807\u5185\u5BB9\uFF1A\u5982\u679C\u4F20 ",paraId:2,tocIndex:4},{value:"null",paraId:2,tocIndex:4},{value:" ",paraId:2,tocIndex:4},{value:"undefined",paraId:2,tocIndex:4},{value:" ",paraId:2,tocIndex:4},{value:"''",paraId:2,tocIndex:4},{value:" \u6216\u4E0D\u4F20\uFF0C\u5219\u4E0D\u663E\u793A\u5FBD\u6807\uFF1B\u5982\u679C\u4F20 ",paraId:2,tocIndex:4},{value:"Badge.dot",paraId:2,tocIndex:4},{value:"\uFF0C\u4F1A\u663E\u793A\u5C0F\u7EA2\u70B9",paraId:2,tocIndex:4},{value:"React.ReactNode | typeof Badge.dot",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"wrapperClassName",paraId:2,tocIndex:4},{value:"Badge",paraId:2,tocIndex:4},{value:" wrap \u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:2,tocIndex:4},{value:"string",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"wrapperStyle",paraId:2,tocIndex:4},{value:"Badge",paraId:2,tocIndex:4},{value:" wrap \u81EA\u5B9A\u4E49\u6837\u5F0F",paraId:2,tocIndex:4},{value:"React.CSSProperties",paraId:2,tocIndex:4},{value:"-",paraId:2,tocIndex:4},{value:"\u5C5E\u6027",paraId:3,tocIndex:5},{value:"\u8BF4\u660E",paraId:3,tocIndex:5},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:5},{value:"\u5168\u5C40\u53D8\u91CF",paraId:3,tocIndex:5},{value:"--color",paraId:3,tocIndex:5},{value:"\u5FBD\u6807\u80CC\u666F\u8272",paraId:3,tocIndex:5},{value:"--right",paraId:3,tocIndex:5},{value:"\u76F8\u5BF9\u4E8E\u6700\u53F3\u8FB9\uFF0C\u5411\u5DE6\u7684\u504F\u79FB\u91CF",paraId:3,tocIndex:5},{value:"0",paraId:3,tocIndex:5},{value:"-",paraId:3,tocIndex:5},{value:"--top",paraId:3,tocIndex:5},{value:"\u76F8\u5BF9\u4E8E\u6700\u4E0A\u8FB9\uFF0C\u5411\u4E0B\u7684\u504F\u79FB\u91CF",paraId:3,tocIndex:5},{value:"0",paraId:3,tocIndex:5},{value:"-",paraId:3,tocIndex:5}]},37814:function(a,e){e.Z=`import classNames from 'classnames'
+import React from 'react'
+import type { FC, ReactNode, CSSProperties } from 'react'
+import { NativeProps, withNativeProps } from '../utils/native-props'
+
+import './style.scss'
+
+const classPrefix = \`triones-antm-badge\`
+
+export const dot = <React.Fragment />
+
+export type BadgeProps = {
+  content?: ReactNode | typeof dot
+  color?: string
+  bordered?: boolean
+  children?: ReactNode
+  wrapperClassName?: string
+  wrapperStyle?: CSSProperties
+} & NativeProps<'--right' | '--top' | '--color'>
+
+export const Badge: FC<BadgeProps> = props => {
+  const { content, color, children } = props
+
+  const isDot = content === dot
+
+  const badgeClass = classNames(classPrefix, {
+    [\`\${classPrefix}-fixed\`]: !!children,
+    [\`\${classPrefix}-dot\`]: isDot,
+    [\`\${classPrefix}-bordered\`]: props.bordered,
+  })
+
+  const element =
+    content || content === 0
+      ? withNativeProps(
+        props,
+        <div
+          className={badgeClass}
+          style={
+            {
+              '--color': color,
+            } as BadgeProps['style']
+          }
+        >
+          {!isDot && (
+            <div className={\`\${classPrefix}-content\`}>{content}</div>
+          )}
+        </div>
+      )
+      : null
+
+  return children ? (
+    <div
+      className={classNames(\`\${classPrefix}-wrapper\`, props.wrapperClassName)}
+      style={props.wrapperStyle}
+    >
+      {children}
+      {element}
+    </div>
+  ) : (
+    element
+  )
+}
+`},76977:function(a,e){e.Z=`.badge-box {
+  background: #eee;
+  width: 40Px;
+  height: 40Px;
+  display: block;
+  border-radius: 8Px;
+  &.dark {
+    background: #666666;
+  }
+}
+
+.badge-demo {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 20Px;
+}
+`},28395:function(a,e){e.Z=`import React from 'react'
+import './base.scss'
+import { DemoBlock } from '../../DemoBlock';
+import Badge from "../index";
+
+export default () => {
+  return (
+    <>
+      <DemoBlock title='\u57FA\u7840\u7528\u6CD5'>
+        <div className='badge-demo'>
+          <Badge content='5'>
+            <div className='badge-box' />
+          </Badge>
+          <Badge content='\u65B0'>
+            <div className='badge-box' />
+          </Badge>
+          <Badge content={Badge.dot}>
+            <div className='badge-box' />
+          </Badge>
+        </div>
+      </DemoBlock>
+
+      <DemoBlock title='\u5E26\u8FB9\u6846'>
+        <Badge content='\u66F4\u65B0\u5566' bordered>
+          <div className='badge-box dark' />
+        </Badge>
+      </DemoBlock>
+
+      <DemoBlock title='\u72EC\u7ACB\u4F7F\u7528'>
+        <div className='badge-demo'>
+          <Badge content='99+' />
+          <Badge content='\u65B0\u6D88\u606F!' />
+        </div>
+      </DemoBlock>
+
+      <DemoBlock title='\u81EA\u5B9A\u4E49\u989C\u8272\u548C\u504F\u79FB\u91CF'>
+        <div className='badge-demo'>
+          <Badge
+            color='#108ee9'
+            content={Badge.dot}
+            style={{ '--right': '100%', '--top': '100%' }}
+          >
+            <div className='badge-box' />
+          </Badge>
+          <Badge
+            color='#87d068'
+            content={Badge.dot}
+            style={{ '--right': '100%' }}
+          >
+            <div className='badge-box' />
+          </Badge>
+          <Badge content={Badge.dot}>
+            <div className='badge-box' />
+          </Badge>
+          <Badge color='orange' content={Badge.dot} style={{ '--top': '100%' }}>
+            <div className='badge-box' />
+          </Badge>
+        </div>
+      </DemoBlock>
+    </>
+  )
+}
+`},29871:function(a,e){e.Z=`import { attachPropertiesToComponent } from '../utils/native-props'
+import { Badge, dot } from './badge'
+export type { BadgeProps } from './badge'
+
+export default attachPropertiesToComponent(Badge, { dot })
+`},50943:function(a,e){e.Z=`$class-prefix-badge: 'triones-antm-badge';
+
+.#{$class-prefix-badge}-wrapper {
+  display: inline-block;
+  position: relative;
+}
+
+.#{$class-prefix-badge} {
+  display: inline-flex;
+  vertical-align: middle;
+  box-sizing: content-box;
+  border-radius: 100px;
+  background-color: var(--color);
+  --right: 0;
+  --top: 0;
+  --color: #ff3141;
+
+  &-content {
+    color: #ffffff;
+    box-sizing: border-box;
+    min-width: 8px;
+    padding: 1px 4px;
+    font-size: 9px;
+    line-height: 12px;
+    white-space: nowrap;
+    font-weight: normal;
+    text-align: center;
+  }
+
+  &-fixed {
+    position: absolute;
+    right: var(--right);
+    top: var(--top);
+    transform: translate(50%, -50%);
+    -webkit-transform: translate(50%, -50%);
+    -moz-transform: translate(50%, -50%);
+    -ms-transform: translate(50%, -50%);
+    -o-transform: translate(50%, -50%);
+  }
+
+  &-dot {
+    min-width: 10px;
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+  }
+
+  &-bordered {
+    border: solid 1px #ffffff;
+  }
+}
+`},21791:function(a,e){e.Z=`.demoBlock {
+  font-size: 14px;
+  margin-bottom: 12px;
+  &:last-of-type {
+    padding-bottom: 32px;
+  }
+  scrollbar-width: thin;
+}
+
+.demoTitle {
+  padding: 12px 12px 8px;
+  color: #697b8c;
+  font-size: 14px;
+  background: #fafbfc;
+}
+
+.demoMain {
+  border-right: none;
+  border-left: none;
+}
+`},7761:function(a,e){e.Z=`import React from 'react'
+import type { FC, ReactNode } from 'react'
+import './index.scss'
+
+interface Props {
+  title: string
+  padding?: string
+  background?: string
+  children?: ReactNode
+}
+
+export const DemoBlock: FC<Props> = props => {
+  return (
+    <div className='demoBlock'>
+      <div className='demoTitle'>{props.title}</div>
+      <div
+        className='demoMain'
+        style={{
+          padding: '12px 12px',
+          background: '#ffffff',
+        }}
+      >
+        {props.children}
+      </div>
+    </div>
+  )
+}
+`},12636:function(a,e){e.Z=`import React, { AriaAttributes } from 'react'
+import type { CSSProperties, ReactElement } from 'react'
+import classNames from 'classnames'
+
+export type NativeProps<S extends string = never> = {
+  className?: string
+  style?: CSSProperties & Partial<Record<S, string>>
+  tabIndex?: number
+} & AriaAttributes
+
+export function withNativeProps<P extends NativeProps>(
+  props: P,
+  element: ReactElement
+) {
+  const p = {
+    ...element.props,
+  }
+  if (props.className) {
+    p.className = classNames(element.props.className, props.className)
+  }
+  if (props.style) {
+    p.style = {
+      ...p.style,
+      ...props.style,
+    }
+  }
+  if (props.tabIndex !== undefined) {
+    p.tabIndex = props.tabIndex
+  }
+  for (const key in props) {
+    if (!props.hasOwnProperty(key)) continue
+    if (key.startsWith('data-') || key.startsWith('aria-')) {
+      p[key] = props[key]
+    }
+  }
+  return React.cloneElement(element, p)
+}
+
+export function attachPropertiesToComponent<C, P extends Record<string, any>>(
+  component: C,
+  properties: P
+): C & P {
+  const ret = component as any
+  for (const key in properties) {
+    if (properties.hasOwnProperty(key)) {
+      ret[key] = properties[key]
+    }
+  }
+  return ret
+}
+
+
+export function toCSSLength(val: string | number) {
+  return typeof val === 'number' ? \`\${val}px\` : val
+}
+`}}]);
