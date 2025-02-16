a17b2ee0de3a97917c5ba1206495d639aa1513e8
落雁沙 <fengxiaotx@163.com>
2025-02-16 22:53:04 +0800

    feat: toast

--- packages/antd-taro-react/src/SpinLoading/spin-loading.tsx
+++ packages/antd-taro-react/src/SpinLoading/spin-loading.tsx
@@ -1,18 +1,37 @@
-import React, {FC, useEffect} from "react";
+import React, {FC} from "react";
 import classNames from "classnames";
+import Base64 from 'crypto-js/enc-base64';
+import Utf8 from 'crypto-js/enc-utf8';
 import "./spin-loading.scss"
-import * as url from "node:url";
+import ReactDOMServer from 'react-dom/server';
+
 
 const spinLoadingCls = 'triones-antm-spin-loading';
 export type SpinLoadingProps = {
   size?: 'small' | 'middle' | 'large' | number
   color?: 'default' | 'primary' | 'white' | string
 }
+
+export function SvgComponent(props: { stroke: string }) {
+  return <svg xmlns="http://www.w3.org/2000/svg" style={{width: '100%', height: '100%'}} viewBox='0 0 32 32'>
+    <circle
+      r="15"
+      cx="16"
+      cy="16"
+      strokeWidth="2"
+      stroke={props.stroke}
+      fill='transparent'
+      strokeLinecap='round'
+      strokeDasharray={15 * 3.14159265358979 * 2}
+      strokeDashoffset={15 * 3.14159265358979 * 0.5}
+    />
+  </svg>
+}
+
 export const SpinLoading: FC<SpinLoadingProps> = ({
                                                     size = 'middle',
                                                     color = 'default'
                                                   }) => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null)
 
   const computedSize = React.useMemo(() => {
     let resSize = null
@@ -45,86 +64,15 @@ export const SpinLoading: FC<SpinLoadingProps> = ({
   }, [color])
 
   const completedStyle: any = React.useMemo(() => {
-    const svg = `<svg className={classNames(\`${spinLoadingCls}-svg\`)} viewBox='0 0 32 32'>
-      <circle className={classNames(\`${spinLoadingCls}-circle\`)}
-        r="15"
-        cx="16"
-        cy="16"
-        strokeWidth="2"
-        fill='transparent'
-        strokeLinecap='round'
-        strokeDasharray={15 * 3.14159265358979 * 2}
-        strokeDashoffset={15 * 3.14159265358979 * 0.5}
-        style={{
-          animation: 'adm-spin-loading-rotate 1s linear infinite',
-          transformOrigin: '50% 50%',
-        }}/>
-    </svg>`
+    const maskImageSvg = ReactDOMServer.renderToString(<SvgComponent stroke={computedColor}/>)
     return {
       '--size': computedSize,
       '--color': computedColor,
+      '--maskImage': `url(data:image/svg+xml;base64,${Base64.stringify(Utf8.parse(maskImageSvg))})`
     }
   }, [computedSize, computedColor])
 
-  useEffect(() => {
-    const canvas = canvasRef.current!;
-    const ctx = canvasRef.current!.getContext('2d')!;
-
-    let angle = 0;
-
-    function draw() {
-      // 清除画布
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      // 设置半圆属性
-      ctx.beginPath();
-      ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, Math.PI, 0, true); // 绘制一个逆时针的半圆
-      ctx.lineWidth = 10;
-      ctx.strokeStyle = 'blue';
-      ctx.stroke();
-
-      // 旋转画布
-      ctx.save();
-      ctx.translate(canvas.width / 2, canvas.height / 2);
-      ctx.rotate(angle);
-
-      // 重新绘制半圆以显示旋转效果
-      ctx.beginPath();
-      ctx.arc(0, 0, canvas.height / 2, Math.PI, 0, true);
-      ctx.lineWidth = 10;
-      ctx.strokeStyle = 'red';
-      ctx.stroke();
-
-      ctx.restore();
-
-      // 更新角度
-      angle += 0.02;
-
-      // 定义圆环的属性
-      // const centerX = canvas.width / 2;
-      // const centerY = canvas.height / 2;
-      // const outerRadius = 100;
-      // const innerRadius = 80;
-      //
-      // // 绘制圆环
-      // ctx.beginPath();
-      // ctx.arc(centerX, centerY, outerRadius, 0, 2 * Math.PI); // 外圆
-      // ctx.arc(centerX, centerY, innerRadius, 0, 2 * Math.PI, true); // 内圆，逆时针
-      // ctx.fillStyle = "red"; // 填充颜色（可选，如果不需要填充则省略此行）
-      // ctx.fill(); // 填充（如果需要）
-      // ctx.lineWidth = 10; // 线条宽度
-      // ctx.strokeStyle = "blue"; // 线条颜色
-      // ctx.stroke(); // 绘制边框
-      // 请求下一帧
-      requestAnimationFrame(draw);
-    }
-
-    // 开始动画
-    draw();
-
-  }, [])
-
   return <div className={classNames(spinLoadingCls)} style={completedStyle}>
-    <canvas ref={canvasRef} width={32} height={32}  />
+    <i  className={classNames(`${spinLoadingCls}-loading`)}/>
   </div>
 }
