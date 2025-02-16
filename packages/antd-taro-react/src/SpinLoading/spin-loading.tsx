4860afcfb9ea774bd979e864a12cc20f01ef7513
落雁沙 <fengxiaotx@163.com>
2025-02-16 19:33:57 +0800

    feat:from

--- packages/antd-taro-react/src/SpinLoading/spin-loading.tsx
+++ packages/antd-taro-react/src/SpinLoading/spin-loading.tsx
@@ -76,47 +76,47 @@ export const SpinLoading: FC<SpinLoadingProps> = ({
       // 清除画布
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      // // 设置半圆属性
-      // ctx.beginPath();
-      // ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, Math.PI, 0, true); // 绘制一个逆时针的半圆
-      // ctx.lineWidth = 10;
-      // ctx.strokeStyle = 'blue';
-      // ctx.stroke();
-      //
-      // // 旋转画布
-      // ctx.save();
-      // ctx.translate(canvas.width / 2, canvas.height / 2);
-      // ctx.rotate(angle);
-      //
-      // // 重新绘制半圆以显示旋转效果
-      // ctx.beginPath();
-      // ctx.arc(0, 0, canvas.height / 2, Math.PI, 0, true);
-      // ctx.lineWidth = 10;
-      // ctx.strokeStyle = 'red';
-      // ctx.stroke();
-      //
-      // ctx.restore();
-      //
-      // // 更新角度
-      // angle += 0.02;
+      // 设置半圆属性
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, Math.PI, 0, true); // 绘制一个逆时针的半圆
+      ctx.lineWidth = 10;
+      ctx.strokeStyle = 'blue';
+      ctx.stroke();
+
+      // 旋转画布
+      ctx.save();
+      ctx.translate(canvas.width / 2, canvas.height / 2);
+      ctx.rotate(angle);
+
+      // 重新绘制半圆以显示旋转效果
+      ctx.beginPath();
+      ctx.arc(0, 0, canvas.height / 2, Math.PI, 0, true);
+      ctx.lineWidth = 10;
+      ctx.strokeStyle = 'red';
+      ctx.stroke();
+
+      ctx.restore();
+
+      // 更新角度
+      angle += 0.02;
 
       // 定义圆环的属性
-      const centerX = canvas.width / 2;
-      const centerY = canvas.height / 2;
-      const outerRadius = 100;
-      const innerRadius = 80;
-
-      // 绘制圆环
-      ctx.beginPath();
-      ctx.arc(centerX, centerY, outerRadius, 0, 2 * Math.PI); // 外圆
-      ctx.arc(centerX, centerY, innerRadius, 0, 2 * Math.PI, true); // 内圆，逆时针
-      ctx.fillStyle = "red"; // 填充颜色（可选，如果不需要填充则省略此行）
-      ctx.fill(); // 填充（如果需要）
-      ctx.lineWidth = 10; // 线条宽度
-      ctx.strokeStyle = "blue"; // 线条颜色
-      ctx.stroke(); // 绘制边框
+      // const centerX = canvas.width / 2;
+      // const centerY = canvas.height / 2;
+      // const outerRadius = 100;
+      // const innerRadius = 80;
+      //
+      // // 绘制圆环
+      // ctx.beginPath();
+      // ctx.arc(centerX, centerY, outerRadius, 0, 2 * Math.PI); // 外圆
+      // ctx.arc(centerX, centerY, innerRadius, 0, 2 * Math.PI, true); // 内圆，逆时针
+      // ctx.fillStyle = "red"; // 填充颜色（可选，如果不需要填充则省略此行）
+      // ctx.fill(); // 填充（如果需要）
+      // ctx.lineWidth = 10; // 线条宽度
+      // ctx.strokeStyle = "blue"; // 线条颜色
+      // ctx.stroke(); // 绘制边框
       // 请求下一帧
-      // requestAnimationFrame(draw);
+      requestAnimationFrame(draw);
     }
 
     // 开始动画
