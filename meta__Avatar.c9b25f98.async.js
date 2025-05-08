5a53933007ded9c4d2f3468c124085ff406368bb
fengxiaotx <fengxiaotx@users.noreply.github.com>
2025-05-08 10:09:32 +0000

    deploy: cf2a0fb9c2f03591d26605c54186eef4a1926de6

---
+++ meta__Avatar.c9b25f98.async.js
@@ -0,0 +1,205 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[3189],{37196:function(a,t,n){var o;n.r(t),n.d(t,{demos:function(){return D}});var c=n(90819),s=n.n(c),l=n(89933),v=n.n(l),_=n(44194),M=n(37701),P=n(80150),i=n(40040),u=n(57180),x=n(12676),e=n(51139),p=n(51865),h=n.n(p),m=n(56368),D={"avatar-demo-base":{component:_.memo(_.lazy(function(){return n.e(5186).then(n.bind(n,39700))})),asset:{type:"BLOCK",id:"avatar-demo-base",refAtomIds:["Avatar"],dependencies:{"index.tsx":{type:"FILE",value:n(40030).Z},react:{type:"NPM",value:"18.3.1"},"@trionesdev/antd-taro-icons-react":{type:"NPM",value:"0.0.1"},"../index.tsx":{type:"FILE",value:n(14551).Z},"../../DemoBlock.tsx":{type:"FILE",value:n(6682).Z},"./style.scss":{type:"FILE",value:n(6145).Z},"./avatar.tsx":{type:"FILE",value:n(78784).Z},classnames:{type:"NPM",value:"2.5.1"},"./index.scss":{type:"FILE",value:n(49933).Z}},entry:"index.tsx"},context:{"../index.tsx":i,"../../DemoBlock.tsx":u,"./style.scss":x,"./avatar.tsx":e,"./index.scss":m,react:o||(o=n.t(_,2)),"@trionesdev/antd-taro-icons-react":P,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Avatar/index.tsx":i,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.tsx":u,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Avatar/style.scss":x,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Avatar/avatar.tsx":e,classnames:p,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.scss":m},renderOpts:{compile:function(){var f=v()(s()().mark(function d(){var I,A=arguments;return s()().wrap(function(r){for(;;)switch(r.prev=r.next){case 0:return r.next=2,n.e(3092).then(n.bind(n,3092));case 2:return r.abrupt("return",(I=r.sent).default.apply(I,A));case 3:case"end":return r.stop()}},d)}));function E(){return f.apply(this,arguments)}return E}()}}}},51139:function(a,t,n){n.r(t),n.d(t,{Avatar:function(){return u}});var o=n(73193),c=n.n(o),s=n(10154),l=n.n(s),v=n(51865),_=n.n(v),M=n(44194),P=n(12676),i=n(31549),u=function(e){var p=e.alt,h=e.icon,m=e.shape,D=m===void 0?"circle":m,f=e.size,E=f===void 0?40:f,d=e.src,I=e.srcSet,A=e.style,O=e.children,r="triones-antm-avatar";return(0,i.jsx)("div",{className:_()(r,l()({},"".concat(r,"-").concat(D!=="square"?"circle":"square"),!0)),style:c()({width:E,height:E,backgroundColor:d?void 0:"#ccc"},A),children:d?typeof d=="string"?(0,i.jsx)("img",{src:d,srcSet:I,alt:p,style:{width:"100%",height:"100%",objectFit:"cover"}}):d:h||O})};t.default=u},40040:function(a,t,n){n.r(t);var o=n(51139);t.default=o.Avatar},57180:function(a,t,n){n.r(t),n.d(t,{DemoBlock:function(){return l}});var o=n(44194),c=n(56368),s=n(31549),l=function(_){return(0,s.jsxs)("div",{className:"demoBlock",children:[(0,s.jsx)("div",{className:"demoTitle",children:_.title}),(0,s.jsx)("div",{className:"demoMain",style:{padding:"12px 12px",background:"#ffffff"},children:_.children})]})}},12676:function(a,t,n){n.r(t)},56368:function(a,t,n){n.r(t)},46611:function(a,t,n){n.r(t),n.d(t,{texts:function(){return c}});var o=n(37701);const c=[{value:"\u6807\u7B7E\u7528\u4E8E\u6807\u8BB0\u548C\u5206\u7C7B\u3002",paraId:0},{value:"\u53C2\u6570",paraId:1,tocIndex:1},{value:"\u8BF4\u660E",paraId:1,tocIndex:1},{value:"\u7C7B\u578B",paraId:1,tocIndex:1},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:1},{value:"alt",paraId:1,tocIndex:1},{value:"\u56FE\u50CF\u65E0\u6CD5\u663E\u793A\u65F6\u7684\u66FF\u4EE3\u6587\u672C",paraId:1,tocIndex:1},{value:"string",paraId:1,tocIndex:1},{value:"-",paraId:1,tocIndex:1},{value:"icon",paraId:1,tocIndex:1},{value:"\u8BBE\u7F6E\u5934\u50CF\u7684\u81EA\u5B9A\u4E49\u56FE\u6807",paraId:1,tocIndex:1},{value:"ReactNode",paraId:1,tocIndex:1},{value:"-",paraId:1,tocIndex:1},{value:"shape",paraId:1,tocIndex:1},{value:"\u6307\u5B9A\u5934\u50CF\u7684\u5F62\u72B6",paraId:1,tocIndex:1},{value:"circle | square",paraId:1,tocIndex:1},{value:"''",paraId:1,tocIndex:1},{value:"size",paraId:1,tocIndex:1},{value:"\u8BBE\u7F6E\u5934\u50CF\u7684\u5927\u5C0F",paraId:1,tocIndex:1},{value:"number | 'large' | 'small' | 'default' | { xs: number, sm: number, ...}",paraId:1,tocIndex:1},{value:"'default'",paraId:1,tocIndex:1},{value:"src",paraId:1,tocIndex:1},{value:"\u56FE\u7247\u7C7B\u5934\u50CF\u7684\u8D44\u6E90\u5730\u5740\u6216\u8005\u56FE\u7247\u5143\u7D20",paraId:1,tocIndex:1},{value:"string | ReactNode",paraId:1,tocIndex:1},{value:"-",paraId:1,tocIndex:1},{value:"srcSet",paraId:1,tocIndex:1},{value:"\u8BBE\u7F6E\u56FE\u7247\u7C7B\u5934\u50CF\u54CD\u5E94\u5F0F\u8D44\u6E90\u5730\u5740",paraId:1,tocIndex:1},{value:"string",paraId:1,tocIndex:1},{value:"-",paraId:1,tocIndex:1}]},78784:function(a,t){t.Z=`import classNames from 'classnames';
+import React from 'react';
+import './style.scss';
+
+type AvatarProps = {
+  alt?: string;
+  icon?: React.ReactNode;
+  shape?: 'circle' | 'square';
+  size?: number;
+  src?: string | React.ReactNode;
+  srcSet?: string;
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+};
+
+export const Avatar: React.FC<AvatarProps> = ({
+  alt,
+  icon,
+  shape = 'circle',
+  size = 40,
+  src,
+  srcSet,
+  style,
+  children,
+}) => {
+  const clsPrefix = 'triones-antm-avatar';
+  return (
+    <div
+      className={classNames(clsPrefix, {
+        [\`\${clsPrefix}-\${shape !== 'square' ? 'circle' : 'square'}\`]: true,
+      })}
+      style={{
+        width: size,
+        height: size,
+        backgroundColor: src ? undefined : '#ccc',
+        ...style,
+      }}
+    >
+      {src ? (
+        typeof src === 'string' ? (
+          <img
+            src={src}
+            srcSet={srcSet}
+            alt={alt}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        ) : (
+          src
+        )
+      ) : icon ? (
+        icon
+      ) : (
+        children
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
+`},40030:function(a,t){t.Z=`import React from 'react';
+import { DemoBlock } from '../../DemoBlock';
+
+import './style.scss'
+import { AntOutline } from '@trionesdev/antd-taro-icons-react';
+import Avatar from "../index";
+
+const url =
+  'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
+
+export default () => {
+  return (
+    <>
+      <DemoBlock title='\u57FA\u672C'>
+        <div className='avatar-content'>
+          <Avatar
+            size={64}
+            icon={<AntOutline style={{ fontSize: '32px', color: '#fff' }} />}
+          />
+          <Avatar
+            icon={<AntOutline style={{ fontSize: '20px', color: '#fff' }} />}
+          />
+          <Avatar
+            size={32}
+            icon={<AntOutline style={{ fontSize: '16px', color: '#fff' }} />}
+          />
+          <Avatar
+            size={24}
+            icon={<AntOutline style={{ fontSize: '12px', color: '#fff' }} />}
+          />
+        </div>
+      </DemoBlock>
+      <DemoBlock title='\u65B9\u5F62'>
+        <div className='avatar-content'>
+          <Avatar
+            size={64}
+            shape="square"
+            icon={<AntOutline style={{ fontSize: '32px', color: '#fff' }} />}
+          />
+          <Avatar
+            shape="square"
+            icon={<AntOutline style={{ fontSize: '20px', color: '#fff' }} />}
+          />
+          <Avatar
+            size={32}
+            shape="square"
+            icon={<AntOutline style={{ fontSize: '16px', color: '#fff' }} />}
+          />
+          <Avatar
+            size={24}
+            shape="square"
+            icon={<AntOutline style={{ fontSize: '12px', color: '#fff' }} />}
+          />
+        </div>
+      </DemoBlock>
+      <DemoBlock title='\u7C7B\u578B'>
+        <div className='avatar-content'>
+          <Avatar>U</Avatar>
+          <Avatar
+            size={48}
+            style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}
+          >
+            USER
+          </Avatar>
+          <Avatar src={url} />
+          <Avatar src={<img src={url} alt="avatar" width={'100%'} />} />
+          <Avatar
+            style={{ backgroundColor: '#87d068' }}
+            icon={<AntOutline style={{ fontSize: '16px', color: '#fff' }} />}
+          />
+        </div>
+      </DemoBlock>
+
+    </>
+  );
+};
+`},14551:function(a,t){t.Z=`import { Avatar } from "./avatar"
+
+export default Avatar `},6145:function(a,t){t.Z=`$avatarCls: 'triones-antm-avatar';
+
+.#{$avatarCls} {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+
+  &-circle {
+    border-radius: 50%;
+  }
+
+  &-shape-square {
+    border-radius: 6Px;
+  }
+}
+`},49933:function(a,t){t.Z=`.demoBlock {
+  font-size: 14px;
+  margin-bottom: 12px;
+  &:last-of-type {
+    padding-bottom: 32px;
+  }
+  scrollbar-width: thin;
+}
+
+.demoTitle {
+  padding: 12px 12px 8px;
+  color: #697b8c;
+  font-size: 14px;
+  background: #fafbfc;
+}
+
+.demoMain {
+  border-right: none;
+  border-left: none;
+}
+`},6682:function(a,t){t.Z=`import React from 'react'
+import type { FC, ReactNode } from 'react'
+import './index.scss'
+
+interface Props {
+  title: string
+  padding?: string
+  background?: string
+  children?: ReactNode
+}
+
+export const DemoBlock: FC<Props> = props => {
+  return (
+    <div className='demoBlock'>
+      <div className='demoTitle'>{props.title}</div>
+      <div
+        className='demoMain'
+        style={{
+          padding: '12px 12px',
+          background: '#ffffff',
+        }}
+      >
+        {props.children}
+      </div>
+    </div>
+  )
+}
+`}}]);
---
+++ meta__Checkbox.d5b7e010.async.js
@@ -0,0 +1,322 @@
+"use strict";(self.webpackChunk_trionesdev_antd_taro_react=self.webpackChunk_trionesdev_antd_taro_react||[]).push([[4059],{16359:function(d,e,n){var l;n.r(e),n.d(e,{demos:function(){return E}});var _=n(90819),i=n.n(_),x=n(89933),C=n.n(x),r=n(44194),s=n(86917),u=n(57180),m=n(96553),o=n(51865),D=n.n(o),k=n(62829),h=n(80150),E={"checkbox-demo-base":{component:r.memo(r.lazy(function(){return n.e(9709).then(n.bind(n,76316))})),asset:{type:"BLOCK",id:"checkbox-demo-base",refAtomIds:["Checkbox"],dependencies:{"index.tsx":{type:"FILE",value:n(96523).Z},react:{type:"NPM",value:"18.3.1"},"../../DemoBlock.tsx":{type:"FILE",value:n(6682).Z},"../checkbox.tsx":{type:"FILE",value:n(50762).Z},classnames:{type:"NPM",value:"2.5.1"},"./index.scss":{type:"FILE",value:n(24274).Z},"@trionesdev/antd-taro-icons-react":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{"../../DemoBlock.tsx":u,"../checkbox.tsx":m,"./index.scss":k,react:l||(l=n.t(r,2)),"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.tsx":u,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Checkbox/checkbox.tsx":m,classnames:o,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Checkbox/index.scss":k,"@trionesdev/antd-taro-icons-react":h},renderOpts:{compile:function(){var p=C()(i()().mark(function f(){var c,v=arguments;return i()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(3092).then(n.bind(n,3092));case 2:return t.abrupt("return",(c=t.sent).default.apply(c,v));case 3:case"end":return t.stop()}},f)}));function b(){return p.apply(this,arguments)}return b}()}},"checkbox-demo-disabled":{component:r.memo(r.lazy(function(){return n.e(9709).then(n.bind(n,49114))})),asset:{type:"BLOCK",id:"checkbox-demo-disabled",refAtomIds:["Checkbox"],dependencies:{"index.tsx":{type:"FILE",value:n(5440).Z},react:{type:"NPM",value:"18.3.1"},"../checkbox.tsx":{type:"FILE",value:n(50762).Z},"../../DemoBlock.tsx":{type:"FILE",value:n(6682).Z},classnames:{type:"NPM",value:"2.5.1"},"./index.scss":{type:"FILE",value:n(24274).Z},"@trionesdev/antd-taro-icons-react":{type:"NPM",value:"0.0.1"}},entry:"index.tsx"},context:{"../checkbox.tsx":m,"../../DemoBlock.tsx":u,"./index.scss":k,react:l||(l=n.t(r,2)),"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Checkbox/checkbox.tsx":m,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/DemoBlock/index.tsx":u,classnames:o,"/home/runner/work/triones-antd-taro/triones-antd-taro/packages/antd-taro-react/src/Checkbox/index.scss":k,"@trionesdev/antd-taro-icons-react":h},renderOpts:{compile:function(){var p=C()(i()().mark(function f(){var c,v=arguments;return i()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,n.e(3092).then(n.bind(n,3092));case 2:return t.abrupt("return",(c=t.sent).default.apply(c,v));case 3:case"end":return t.stop()}},f)}));function b(){return p.apply(this,arguments)}return b}()}}}},96553:function(d,e,n){n.r(e),n.d(e,{Checkbox:function(){return D}});var l=n(10154),_=n.n(l),i=n(45332),x=n.n(i),C=n(62829),r=n(51865),s=n.n(r),u=n(44194),m=n(80150),o=n(31549),D=function(h){var E=h.children,p=h.checked,b=h.shape,f=b===void 0?"round":b,c=h.disabled,v=(0,u.useState)(p),P=x()(v,2),t=P[0],g=P[1],a="triones-antm-checkbox-item";return(0,o.jsx)("label",{className:s()(a),children:(0,o.jsxs)("div",{className:s()("".concat(a,"-container")),style:{pointerEvents:c?"none":"auto"},children:[f==="round"&&(0,o.jsxs)(o.Fragment,{children:[(0,o.jsxs)("div",{className:s()("".concat(a,"-wrap")),children:[(0,o.jsx)("span",{className:s()("".concat(a,"-base")),children:(0,o.jsx)("input",{className:s()("".concat(a,"-input")),type:"checkbox",onChange:function(M){g(M.target.checked)}})}),(0,o.jsx)("div",{className:s()("".concat(a,"-fake")),children:(0,o.jsx)(m.CheckOutline,{className:s()("".concat(a,"-fake-icon"),{checkedIcon:!c&&t,uncheckedIcon:!c&&!t,disabledCheckedIcon:c&&t,disabledUncheckedIcon:c&&!t})})})]}),(0,o.jsx)("div",{className:s()("".concat(a,"-content"),_()({},"".concat(a,"-content-disabled"),c)),children:E})]}),f==="button"&&(0,o.jsxs)("div",{className:s()("".concat(a,"-button"),_()(_()({},"".concat(a,"-button-disabled"),c),"".concat(a,"-button-checked"),t)),onClick:function(){g(!t)},children:[(0,o.jsx)("div",{className:s()("".concat(a,"-button-content")),children:E}),t&&(0,o.jsx)("div",{className:s()("".concat(a,"-button-icon")),children:(0,o.jsx)(m.CheckOutline,{className:s()("checkedIcon")})})]})]})})}},57180:function(d,e,n){n.r(e),n.d(e,{DemoBlock:function(){return x}});var l=n(44194),_=n(56368),i=n(31549),x=function(r){return(0,i.jsxs)("div",{className:"demoBlock",children:[(0,i.jsx)("div",{className:"demoTitle",children:r.title}),(0,i.jsx)("div",{className:"demoMain",style:{padding:"12px 12px",background:"#ffffff"},children:r.children})]})}},62829:function(d,e,n){n.r(e)},56368:function(d,e,n){n.r(e)},54179:function(d,e,n){n.r(e),n.d(e,{texts:function(){return _}});var l=n(86917);const _=[]},50762:function(d,e){e.Z=`import "./index.scss"
+import classNames from "classnames";
+import React, {FC, useState} from "react";
+import {CheckOutline} from "@trionesdev/antd-taro-icons-react";
+
+type CheckboxProps = {
+  children?: React.ReactNode;
+  checked?: boolean;
+  shape?: 'button' | 'round'
+  disabled?: boolean
+}
+export const Checkbox: FC<CheckboxProps> = ({children, checked, shape = 'round', disabled}) => {
+  const [innerChecked, setInnerChecked] = useState(checked)
+  const cls = 'triones-antm-checkbox-item';
+  return <label className={classNames(cls)}>
+    <div className={classNames(\`\${cls}-container\`)} style={{pointerEvents: disabled ? 'none' : 'auto'}}>
+      {shape === 'round' && <>
+        <div className={classNames(\`\${cls}-wrap\`)}>
+          <span className={classNames(\`\${cls}-base\`)}>
+            <input className={classNames(\`\${cls}-input\`)} type={"checkbox"}
+                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                     setInnerChecked(e.target.checked)
+                   }}/>
+          </span>
+
+          <div className={classNames(\`\${cls}-fake\`)}>
+            <CheckOutline className={classNames(\`\${cls}-fake-icon\`, {
+              'checkedIcon': !disabled && innerChecked,
+              'uncheckedIcon': !disabled && !innerChecked,
+              'disabledCheckedIcon': disabled && innerChecked,
+              'disabledUncheckedIcon': disabled && !innerChecked,
+            })}/>
+          </div>
+        </div>
+        <div className={classNames(\`\${cls}-content\`, {[\`\${cls}-content-disabled\`]: disabled})}>{children}</div>
+      </>}
+      {shape === 'button' && <div className={classNames(\`\${cls}-button\`, {
+        [\`\${cls}-button-disabled\`]: disabled,
+        [\`\${cls}-button-checked\`]: innerChecked
+      })} onClick={() => {
+        setInnerChecked(!innerChecked)
+      }}>
+        <div className={classNames(\`\${cls}-button-content\`)}>{children}</div>
+        {innerChecked && <div className={classNames(\`\${cls}-button-icon\`)}>
+          <CheckOutline className={classNames(\`checkedIcon\`)}/>
+        </div>}
+      </div>}
+    </div>
+  </label>
+}
+`},96523:function(d,e){e.Z=`import { DemoBlock } from '../../DemoBlock';
+import React from "react";
+import {Checkbox} from "../checkbox";
+
+export default () => {
+  return <>
+    <DemoBlock title='\u57FA\u7840\u7528\u6CD5'>
+      <Checkbox checked={true}>\u82F9\u679C</Checkbox>
+      <Checkbox checked={false}>\u9999\u8549</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='\u9ED8\u8BA4\u9009\u4E2D'>
+      <Checkbox checked={true}>\u82F9\u679C</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='\u7981\u7528'>
+      <Checkbox checked={true} disabled={true}>\u82F9\u679C</Checkbox>
+      <Checkbox checked={false} disabled={true}>\u9999\u8549</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='Button'>
+      <Checkbox checked={true} shape={'button'}>\u82F9\u679C</Checkbox>
+    </DemoBlock>
+  </>
+}
+`},5440:function(d,e){e.Z=`import { DemoBlock } from '../../DemoBlock';
+import React from "react";
+import {Checkbox} from "../checkbox";
+
+export default () => {
+  return <>
+    <DemoBlock title='\u7981\u7528'>
+      <Checkbox checked={true} disabled={true}>\u82F9\u679C</Checkbox>
+      <Checkbox checked={false} disabled={true}>\u9999\u8549</Checkbox>
+    </DemoBlock>
+    <DemoBlock title='Button'>
+      <Checkbox checked={true} shape={'button'} disabled={true}>\u82F9\u679C</Checkbox>
+    </DemoBlock>
+  </>
+}
+`},24274:function(d,e){e.Z=`$checkboxItemCls: 'triones-antm-checkbox-item';
+
+.#{$checkboxItemCls} {
+  display: inline-block;
+  line-height: 1.8;
+  margin-right: 8Px;
+  box-sizing: border-box;
+
+  &-container {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+  }
+
+  &-wrap {
+    position: relative;
+    width: 22Px;
+    height: 22Px;
+    flex: 0 0 22Px;
+  }
+
+  &-base {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    box-sizing: border-box;
+
+    input {
+      position: absolute;
+      top: 0;
+      left: 0;
+      opacity: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 2;
+      border: none;
+      overflow: visible;
+      box-sizing: border-box;
+      padding: 0;
+      margin: 0;
+    }
+
+    &-icon {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      pointer-events: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #1677ff;
+      background-color: currentcolor;
+    }
+  }
+
+  &-fake {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    pointer-events: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &-icon {
+      border-radius: 50vh;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      &::before {
+        //font-family: "triones-ant-mini-icon" !important;
+        font-style: normal;
+        line-height: 1;
+      }
+    }
+
+    .checkedIcon {
+      background-color: #1677ff;
+      color: #ffffff;
+      &::before {
+        font-style: normal;
+        //content: "\\e75f";
+      }
+    }
+
+    .uncheckedIcon {
+      background-color: #ffffff;
+      border: 1Px solid #cccccc;
+      border-radius: 50vh;
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+      &::before {
+        font-style: normal;
+        content: " ";
+      }
+    }
+
+    .disabledCheckedIcon {
+      box-sizing: border-box;
+      border: 1Px solid #cccccc;
+      border-radius: 50vh;
+      width: 100%;
+      height: 100%;
+      background-color: #f5f5f5;
+      color: #cccccc;
+
+      &::before {
+        font-style: normal;
+        //content: "\\e75f";
+      }
+    }
+
+    .disabledUncheckedIcon {
+      box-sizing: border-box;
+      border: 1Px solid #cccccc;
+      border-radius: 50vh;
+      width: 100%;
+      height: 100%;
+      background-color: #f5f5f5;
+      &::before {
+        font-style: normal;
+        content: " ";
+      }
+    }
+  }
+
+  &-content {
+    font-size: 17Px;
+    margin-left: 4Px;
+
+    &-disabled {
+      opacity: 0.4;
+      pointer-events: none;
+    }
+  }
+
+  &-button {
+    padding: 4Px 12Px;
+    border-radius: 8Px;
+    position: relative;
+    overflow: hidden;
+    background-color: #f6f6f6;
+    box-sizing: border-box;
+
+    &-checked {
+      background-color: #ffffff;
+      border: 1Px solid #1677ff;
+
+      .#{$checkboxItemCls}-button-content {
+        color: #1677ff;
+      }
+    }
+
+    &-disabled {
+      opacity: 0.4;
+      pointer-events: none;
+    }
+
+    &-content {
+      //color: #1677ff;
+    }
+
+    &-icon {
+      align-items: flex-end;
+      border-bottom: 10Px solid var(--nutui-color-primary, #1677ff);
+      border-left: 10Px solid transparent;
+      border-right: 10Px solid var(--nutui-color-primary, #1677ff);
+      border-top: 10Px solid transparent;
+      bottom: 0;
+      display: flex;
+      height: 0;
+      justify-content: flex-end;
+      position: absolute;
+      right: 0;
+      width: 0;
+
+      .checkedIcon {
+        font-size: 6Px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(1Px, -1Px);
+        //font-family: "triones-ant-mini-icon" !important;
+        font-style: normal;
+        color: #ffffff;
+
+        &::before {
+          font-style: normal;
+          //content: "\\e75f";
+        }
+      }
+    }
+  }
+}
+`},6682:function(d,e){e.Z=`import React from 'react'
+import type { FC, ReactNode } from 'react'
+import './index.scss'
+
+interface Props {
+  title: string
+  padding?: string
+  background?: string
+  children?: ReactNode
+}
+
+export const DemoBlock: FC<Props> = props => {
+  return (
+    <div className='demoBlock'>
+      <div className='demoTitle'>{props.title}</div>
+      <div
+        className='demoMain'
+        style={{
+          padding: '12px 12px',
+          background: '#ffffff',
+        }}
+      >
+        {props.children}
+      </div>
+    </div>
+  )
+}
+`}}]);
