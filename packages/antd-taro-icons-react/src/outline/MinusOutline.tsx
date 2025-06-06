13e4cff89cc0bf599f95134accc7b87f034549fc
落雁沙 <fengxiaotx@163.com>
2025-06-06 21:47:05 +0800

    feat: update icons

--- packages/antd-taro-icons-react/src/outline/MinusOutline.tsx
+++ packages/antd-taro-icons-react/src/outline/MinusOutline.tsx
@@ -5,3 +5,4 @@ import classNames from "classnames";
 export const MinusOutline: FC<IconProps> = ({className, style,...rest}) => {
   return <Icon {...rest} className={classNames([`${IconClsPrefix}MinusOutline`,className])} style={style}/>
 }
+  
