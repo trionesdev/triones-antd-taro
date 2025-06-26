056a2179080e1632dcb220ea2b798c0d4f8751dc
bane.shi <fengxiaotx@163.com>
2025-06-26 18:09:20 +0800

    feat:

--- packages/antd-taro-react/src/Space/space.tsx
+++ packages/antd-taro-react/src/Space/space.tsx
@@ -1,21 +1,22 @@
-import {CSSProperties, FC, ReactNode, useMemo} from 'react'
-import React from 'react'
-import classNames from 'classnames'
-import './style.scss'
+import classNames from 'classnames';
+import React, { CSSProperties, FC, ReactNode, useMemo } from 'react';
+import './style.scss';
 
-const classPrefix = `triones-antm-space`
+const classPrefix = `triones-antm-space`;
 
-export type Size = 'small' | 'middle' | 'large' | number
+export type Size = 'small' | 'middle' | 'large' | number;
 
 export type SpaceProps = {
+  className?: string;
+
   /**
    * @description 间距方向
    */
-  direction?: 'horizontal' | 'vertical'
+  direction?: 'horizontal' | 'vertical';
   /**
    * @description 垂直对齐方式
    */
-  align?: 'start' | 'end' | 'center' | 'baseline'
+  align?: 'start' | 'end' | 'center' | 'baseline';
   /**
    * @description 垂直对齐方式
    */
@@ -30,86 +31,87 @@ export type SpaceProps = {
   /**
    * @description 换行
    */
-  wrap?: boolean
+  wrap?: boolean;
   /**
    * @description 是否为块级元素
    */
-  block?: boolean
+  block?: boolean;
   /**
    * @description 点击事件
    * @param event
    */
-  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
-  children?: ReactNode
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  children?: ReactNode;
   /**
    * @description 间距大小
    */
-  size?: Size | Size[]
-  style?: CSSProperties
-}
-
+  size?: Size | Size[];
+  style?: CSSProperties;
+};
 
 export const Space: FC<SpaceProps> = ({
-                                        children,
-                                        direction = 'horizontal',
-                                        align,
-                                        justify,
-                                        wrap,
-                                        block,
-                                        onClick,
-                                        size = 'small',
-                                        style,
-                                      }) => {
-
+  children,
+  className,
+  direction = 'horizontal',
+  align,
+  justify,
+  wrap,
+  block,
+  onClick,
+  size = 'small',
+  style,
+}) => {
   const sizeValue = (size: Size) => {
     if (typeof size === 'number') {
-      return `${size}Px`
+      return `${size}Px`;
     } else if (size === 'small') {
-      return '8Px'
+      return '8Px';
     } else if (size === 'middle') {
-      return '16Px'
+      return '16Px';
     } else if (size === 'large') {
-      return '24Px'
+      return '24Px';
     }
-  }
+  };
   const columnGapValue = useMemo(() => {
     if (Array.isArray(size)) {
-      return sizeValue(size[1])
+      return sizeValue(size[1]);
     } else {
-      return sizeValue(size)
+      return sizeValue(size);
     }
-  }, [size])
+  }, [size]);
 
   const rowGapValue = useMemo(() => {
     if (Array.isArray(size)) {
-      return sizeValue(size[1])
+      return sizeValue(size[1]);
     } else {
-      return sizeValue(size)
+      return sizeValue(size);
     }
-  }, [size])
+  }, [size]);
 
-  return (<div
-    className={classNames(classPrefix, {
-      [`${classPrefix}-wrap`]: wrap,
-      [`${classPrefix}-block`]: block,
-      [`${classPrefix}-${direction}`]: true,
-      [`${classPrefix}-align-${align}`]: !!align,
-      [`${classPrefix}-justify-${justify}`]: !!justify,
-    })}
-    style={{
-      ...style,
-      columnGap: columnGapValue,
-      rowGap: rowGapValue
-    }}
-    onClick={onClick}
-  >
-    {React.Children.map(children, child => {
-      return (
-        child !== null &&
-        child !== undefined && (
-          <div className={`${classPrefix}-item`}>{child}</div>
-        )
-      )
-    })}
-  </div>)
-}
+  return (
+    <div
+      className={classNames(classPrefix, className, {
+        [`${classPrefix}-wrap`]: wrap,
+        [`${classPrefix}-block`]: block,
+        [`${classPrefix}-${direction}`]: true,
+        [`${classPrefix}-align-${align}`]: !!align,
+        [`${classPrefix}-justify-${justify}`]: !!justify,
+      })}
+      style={{
+        ...style,
+        columnGap: columnGapValue,
+        rowGap: rowGapValue,
+      }}
+      onClick={onClick}
+    >
+      {React.Children.map(children, (child) => {
+        return (
+          child !== null &&
+          child !== undefined && (
+            <div className={`${classPrefix}-item`}>{child}</div>
+          )
+        );
+      })}
+    </div>
+  );
+};
