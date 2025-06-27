ae38cd26cd9a6cdb601ed72ab94e8f0ccffbacd5
落雁沙 <fengxiaotx@163.com>
2025-06-27 08:25:18 +0800

    feat: update

--- packages/antd-taro-react/src/Descriptions/Descriptions.tsx
+++ packages/antd-taro-react/src/Descriptions/Descriptions.tsx
@@ -1,10 +1,22 @@
 import React, {FC, PropsWithChildren} from "react"
+import "/style.scss"
+import classNames from "classnames";
+
 
 type DescriptionsProps={
-  column?:number
+  columns?:number
   layout?:'horizontal' | 'vertical'
 }
 
-export const Descriptions:FC<PropsWithChildren<DescriptionsProps>> = ({}) => {
-  return <div></div>
+
+
+export const Descriptions:FC<PropsWithChildren<DescriptionsProps>> = ({children,columns=1}) => {
+
+  const handleItemsRender=()=>{
+    return <></>
+  }
+
+  return <div className={classNames(cls)}>
+    {handleItemsRender()}
+  </div>
 }
---
+++ packages/antd-taro-react/src/Descriptions/types.ts
@@ -0,0 +1,1 @@
+const cls = `triones-antm-descriptions`
