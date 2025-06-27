9703210e68fdef314d5a94a1c8e1986a609b5829
落雁沙 <fengxiaotx@163.com>
2025-06-27 08:02:45 +0800

    feat: update

---
+++ packages/antd-taro-react/src/Descriptions/demo/base.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {DemoBlock} from "../../DemoBlock";
+import Descriptions from "../index";
+
+export default () => {
+  return <div>
+    <DemoBlock title={`基本使用`}>
+      <Descriptions></Descriptions>
+    </DemoBlock>
+  </div>
+}
---
+++ packages/antd-taro-react/src/Descriptions/index.md
@@ -0,0 +1,18 @@
+---
+category: Components
+group:
+  title: 反馈
+  order: 6
+---
+# Descriptions 描述列表
+
+ 
+
+## 何时使用
+
+
+## 示例
+
+<code src="./demo/base.tsx"></code>
+
+## 属性
