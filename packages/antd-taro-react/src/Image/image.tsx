32abf9d1b6037260be5c4b37f38bb7532d30221a
bane.shi <fengxiaotx@163.com>
2025-10-13 22:59:41 +0800

    feat: save update

--- packages/antd-taro-react/src/Image/image.tsx
+++ packages/antd-taro-react/src/Image/image.tsx
@@ -1,7 +1,17 @@
-import {Image as TaroImage,ImageProps as TaroImageProps} from "@tarojs/components"
+import {Image as TaroImage, ImageProps as TaroImageProps} from "@tarojs/components"
 import React from "react";
 import {FC} from "react";
-export type ImageProps = TaroImageProps
-export const Image:FC<ImageProps> = ({...rest}) => {
+
+export type ImageProps = Omit<TaroImageProps, 'style'> & {
+  style?: React.CSSProperties
+  width?: string | number
+  height?: string | number
+}
+export const Image: FC<ImageProps> = ({...rest}) => {
+  rest.style = {
+    ...rest.style,
+    width: rest.width,
+    height: rest.height
+  }
   return <TaroImage {...rest}/>
 }
